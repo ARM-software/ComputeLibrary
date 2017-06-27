443c8b979518ad494b9e59648cbd061ebf37cba9
Moritz Pflanzer <moritz.pflanzer@arm.com>
2017-06-27 12:36:21 +0100

    COMPMID-417: Move ActivationLayer tests to boost
    This is only a quick&dirty solution to prevent the nightlies from
    failing. Clean up has to be done under COMPMID-426.
    
    Change-Id: Ife1a377efa6aaf389a36f1f8d913ad601b3fdb3c
    Reviewed-on: http://mpd-gerrit.cambridge.arm.com/78966
    Tested-by: Kaizen <jeremy.johnson+kaizengerrit@arm.com>
    Reviewed-by: Michele DiGiorgio <michele.digiorgio@arm.com>
    Reviewed-by: Georgios Pinitas <georgios.pinitas@arm.com>

--- tests/validation/CL/ActivationLayer.cpp
+++ tests/validation/CL/ActivationLayer.cpp
@@ -21,9 +21,9 @@
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
  * SOFTWARE.
  */
+#include "CL/CLAccessor.h"
+#include "CL/Helper.h"
 #include "Globals.h"
-#include "NEON/Helper.h"
-#include "NEON/NEAccessor.h"
 #include "PaddingCalculator.h"
 #include "TensorLibrary.h"
 #include "TypePrinter.h"
@@ -35,9 +35,9 @@
 
 #include "arm_compute/core/Helpers.h"
 #include "arm_compute/core/Types.h"
-#include "arm_compute/runtime/NEON/functions/NEActivationLayer.h"
-#include "arm_compute/runtime/Tensor.h"
-#include "arm_compute/runtime/TensorAllocator.h"
+#include "arm_compute/runtime/CL/CLTensor.h"
+#include "arm_compute/runtime/CL/CLTensorAllocator.h"
+#include "arm_compute/runtime/CL/functions/CLActivationLayer.h"
 
 #include "boost_wrapper.h"
 
@@ -47,7 +47,7 @@
 
 using namespace arm_compute;
 using namespace arm_compute::test;
-using namespace arm_compute::test::neon;
+using namespace arm_compute::test::cl;
 using namespace arm_compute::test::validation;
 
 namespace
@@ -74,7 +74,7 @@ float activation_layer_tolerance(ActivationLayerInfo::ActivationFunction activat
     }
 }
 
-/** Compute Neon activation layer function.
+/** Compute CL activation layer function.
  *
  * @param[in] in_place             Compute the activation layer in-place.
  * @param[in] shape                Shape of the input and output tensors.
@@ -84,14 +84,14 @@ float activation_layer_tolerance(ActivationLayerInfo::ActivationFunction activat
  *
  * @return Computed output tensor.
  */
-Tensor compute_activation_layer(bool in_place, const TensorShape &shape, DataType dt, ActivationLayerInfo act_info, int fixed_point_position = 0)
+CLTensor compute_activation_layer(bool in_place, const TensorShape &shape, DataType dt, ActivationLayerInfo act_info, int fixed_point_position = 0)
 {
     // Create tensors
-    Tensor src = create_tensor(shape, dt, 1, fixed_point_position);
-    Tensor dst = create_tensor(shape, dt, 1, fixed_point_position);
+    CLTensor src = create_tensor(shape, dt, 1, fixed_point_position);
+    CLTensor dst = create_tensor(shape, dt, 1, fixed_point_position);
 
     // Create and configure function
-    NEActivationLayer act_layer;
+    CLActivationLayer act_layer;
 
     if(in_place)
     {
@@ -119,7 +119,7 @@ Tensor compute_activation_layer(bool in_place, const TensorShape &shape, DataTyp
         float max_bound = 0;
         std::tie(min_bound, max_bound) = get_activation_layer_test_bounds<float>(act_info.activation());
         std::uniform_real_distribution<> distribution(min_bound, max_bound);
-        library->fill(NEAccessor(src), distribution, 0);
+        library->fill(CLAccessor(src), distribution, 0);
     }
     else
     {
@@ -127,7 +127,7 @@ Tensor compute_activation_layer(bool in_place, const TensorShape &shape, DataTyp
         int max_bound = 0;
         std::tie(min_bound, max_bound) = get_activation_layer_test_bounds<int8_t>(act_info.activation(), fixed_point_position);
         std::uniform_int_distribution<> distribution(min_bound, max_bound);
-        library->fill(NEAccessor(src), distribution, 0);
+        library->fill(CLAccessor(src), distribution, 0);
     }
 
     // Compute function
@@ -145,24 +145,24 @@ Tensor compute_activation_layer(bool in_place, const TensorShape &shape, DataTyp
 } // namespace
 
 #ifndef DOXYGEN_SKIP_THIS
-BOOST_AUTO_TEST_SUITE(NEON)
+BOOST_AUTO_TEST_SUITE(CL)
 BOOST_AUTO_TEST_SUITE(ActivationLayer)
 
 BOOST_TEST_DECORATOR(*boost::unit_test::label("precommit") * boost::unit_test::label("nightly"))
-BOOST_DATA_TEST_CASE(Configuration, boost::unit_test::data::make({ false, true }) * (SmallShapes() + LargeShapes()) * CNNDataTypes(), in_place, shape, dt)
+BOOST_DATA_TEST_CASE(Configuration, boost::unit_test::data::make({ false, true }) * (SmallShapes() + LargeShapes()) * CNNFloatDataTypes(), in_place, shape, dt)
 {
     // Set fixed point position data type allowed
     const int fixed_point_position = (arm_compute::is_data_type_fixed_point(dt)) ? 3 : 0;
 
     // Create tensors
-    Tensor src = create_tensor(shape, dt, 1, fixed_point_position);
-    Tensor dst = create_tensor(shape, dt, 1, fixed_point_position);
+    CLTensor src = create_tensor(shape, dt, 1, fixed_point_position);
+    CLTensor dst = create_tensor(shape, dt, 1, fixed_point_position);
 
     BOOST_TEST(src.info()->is_resizable());
     BOOST_TEST(dst.info()->is_resizable());
 
     // Create and configure function
-    NEActivationLayer act_layer;
+    CLActivationLayer act_layer;
 
     if(in_place)
     {
@@ -200,13 +200,13 @@ BOOST_DATA_TEST_CASE(RunSmall, boost::unit_test::data::make({ false, true }) * S
     ActivationLayerInfo act_info(act_function, 1.f, 1.f);
 
     // Compute function
-    Tensor dst = compute_activation_layer(in_place, shape, dt, act_info);
+    CLTensor dst = compute_activation_layer(in_place, shape, dt, act_info);
 
     // Compute reference
     RawTensor ref_dst = Reference::compute_reference_activation_layer(shape, dt, act_info);
 
     // Validate output
-    validate(NEAccessor(dst), ref_dst, activation_layer_tolerance(act_function));
+    validate(CLAccessor(dst), ref_dst, activation_layer_tolerance(act_function));
 }
 
 BOOST_TEST_DECORATOR(*boost::unit_test::label("nightly"))
@@ -216,35 +216,13 @@ BOOST_DATA_TEST_CASE(RunLarge, boost::unit_test::data::make({ false, true }) * L
     ActivationLayerInfo act_info(act_function, 1.f, 1.f);
 
     // Compute function
-    Tensor dst = compute_activation_layer(in_place, shape, dt, act_info);
+    CLTensor dst = compute_activation_layer(in_place, shape, dt, act_info);
 
     // Compute reference
     RawTensor ref_dst = Reference::compute_reference_activation_layer(shape, dt, act_info);
 
     // Validate output
-    validate(NEAccessor(dst), ref_dst, activation_layer_tolerance(act_function));
-}
-BOOST_AUTO_TEST_SUITE_END()
-
-/** @note We test for fixed point precision [3,5] because [1,2] and [6,7] ranges
- *        cause overflowing issues in most of the transcendentals functions.
- */
-BOOST_AUTO_TEST_SUITE(Quantized)
-BOOST_TEST_DECORATOR(*boost::unit_test::label("precommit"))
-BOOST_DATA_TEST_CASE(RunSmall, boost::unit_test::data::make({ false, true }) * SmallShapes() * ActivationFunctions() * boost::unit_test::data::xrange(3, 6, 1),
-                     in_place, shape, act_function, fixed_point_position)
-{
-    // Create activation layer info
-    ActivationLayerInfo act_info(act_function, 1.f, 1.f);
-
-    // Compute function
-    Tensor dst = compute_activation_layer(in_place, shape, DataType::QS8, act_info, fixed_point_position);
-
-    // Compute reference
-    RawTensor ref_dst = Reference::compute_reference_activation_layer(shape, DataType::QS8, act_info, fixed_point_position);
-
-    // Validate output
-    validate(NEAccessor(dst), ref_dst, activation_layer_tolerance(act_function, fixed_point_position));
+    validate(CLAccessor(dst), ref_dst, activation_layer_tolerance(act_function));
 }
 BOOST_AUTO_TEST_SUITE_END()
 
