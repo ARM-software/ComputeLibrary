75af28e6936f084fdfa65add7631cd2cd2050b0c
Gian Marco Iodice <gianmarco.iodice@arm.com>
2017-08-21 09:15:58 +0100

    COMPMID-417 - Fixed call of direct convolution 1x1 for bifrost
    Change-Id: Ic4e56e8881b8c66758e67c486514ec397cf43f8e
    Reviewed-on: http://mpd-gerrit.cambridge.arm.com/84592
    Reviewed-by: Moritz Pflanzer <moritz.pflanzer@arm.com>
    Reviewed-by: Anthony Barbier <anthony.barbier@arm.com>
    Tested-by: Kaizen <jeremy.johnson+kaizengerrit@arm.com>

--- src/core/CL/kernels/CLDirectConvolutionLayerKernel.cpp
+++ src/core/CL/kernels/CLDirectConvolutionLayerKernel.cpp
@@ -108,7 +108,7 @@ void CLDirectConvolutionLayerKernel::configure(const ICLTensor *input, const ICL
         options.emplace("-DHAS_BIAS");
     }
 
-    if((gpu_target == GPUTarget::BIFROST) && (kernel_size <= 5) && (_conv_stride_x == 1) && (_conv_stride_y == 1) && (input->info()->data_type() == DataType::F32))
+    if((gpu_target == GPUTarget::BIFROST) && (kernel_size <= 5) && (kernel_size != 1) && (_conv_stride_x == 1) && (_conv_stride_y == 1) && (input->info()->data_type() == DataType::F32))
     {
         options.emplace("-DWEIGHTS_DEPTH=" + support::cpp11::to_string(_weights->info()->dimension(2)));
 
