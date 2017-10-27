93dcd83caacc01eef99c550bd50a8d5393e55f1c
Georgios Pinitas <georgios.pinitas@arm.com>
2017-10-27 12:48:20 +0100

    COMPMID-645: MobileNet network and benchmark
    Change-Id: Id9c50abdbd28e7e95fd460240cec51af1d6f1036
    Reviewed-on: http://mpd-gerrit.cambridge.arm.com/95580
    Reviewed-by: Anthony Barbier <anthony.barbier@arm.com>
    Tested-by: Kaizen <jeremy.johnson+kaizengerrit@arm.com>

---
+++ tests/benchmark/fixtures/MobileNetFixture.h
@@ -0,0 +1,79 @@
+/*
+ * Copyright (c) 2017 ARM Limited.
+ *
+ * SPDX-License-Identifier: MIT
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to
+ * deal in the Software without restriction, including without limitation the
+ * rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+ * sell copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+#ifndef ARM_COMPUTE_TEST_MOBILENETFIXTURE
+#define ARM_COMPUTE_TEST_MOBILENETFIXTURE
+
+#include "tests/AssetsLibrary.h"
+#include "tests/Utils.h"
+#include "tests/framework/Fixture.h"
+#include "tests/networks/MobileNetNetwork.h"
+
+namespace arm_compute
+{
+namespace test
+{
+template <typename TensorType,
+          typename Accessor,
+          typename ActivationLayerFunction,
+          typename ConvolutionLayerFunction,
+          typename DirectConvolutionLayerFunction,
+          typename DepthwiseConvolutionFunction,
+          typename ReshapeFunction,
+          typename PoolingLayerFunction>
+class MobileNetFixture : public framework::Fixture
+{
+public:
+    template <typename...>
+    void setup(int batches)
+    {
+        network.init(batches);
+        network.build();
+        network.allocate();
+        network.fill_random();
+    }
+
+    void run()
+    {
+        network.run();
+    }
+
+    void teardown()
+    {
+        network.clear();
+    }
+
+private:
+    networks::MobileNetNetwork<TensorType,
+             Accessor,
+             ActivationLayerFunction,
+             ConvolutionLayerFunction,
+             DirectConvolutionLayerFunction,
+             DepthwiseConvolutionFunction,
+             ReshapeFunction,
+             PoolingLayerFunction>
+             network{};
+};
+} // namespace test
+} // namespace arm_compute
+#endif /* ARM_COMPUTE_TEST_MOBILENETFIXTURE */
