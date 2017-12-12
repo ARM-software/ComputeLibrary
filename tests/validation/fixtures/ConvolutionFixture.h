c79519648b9e908c26cefc436a3c34b83f36403c
Georgios Pinitas <georgios.pinitas@arm.com>
2017-12-12 17:58:11 +0000

    COMPMID-556: Move Convolution to reference folder
    Change-Id: I5e7f2d9390b45be252d5f692b47f88472b681d14
    Reviewed-on: https://eu-gerrit-1.euhpc.arm.com/112993
    Reviewed-by: Anthony Barbier <anthony.barbier@arm.com>
    Tested-by: Anthony Barbier <anthony.barbier@arm.com>

--- tests/validation/fixtures/ConvolutionFixture.h
+++ tests/validation/fixtures/ConvolutionFixture.h
@@ -31,7 +31,7 @@
 #include "tests/IAccessor.h"
 #include "tests/framework/Asserts.h"
 #include "tests/framework/Fixture.h"
-#include "tests/validation/CPP/Convolution.h"
+#include "tests/validation/reference/Convolution.h"
 
 #include <random>
 
