1f03ca2eb8f28d9bf89d16bae4cd646cbea4f422
Håkan x Ardö <hakanad@axis.com>
2018-01-31 10:55:56 +0100

    Bugfix for NumPyBinLoader with SubTensor output.
    When a SubTensor is used as output from a NumPyBinLoader, data should
    not be written to the begining of the buffer.
    
    Change-Id: I6ed4b24710ac09b41ca92c7e21f24d44a3ed2881

--- utils/GraphUtils.cpp
+++ utils/GraphUtils.cpp
@@ -352,7 +352,7 @@ bool NumPyBinLoader::access_tensor(ITensor &tensor)
     if(tensor.info()->padding().empty())
     {
         // If tensor has no padding read directly from stream.
-        stream.read(reinterpret_cast<char *>(tensor.buffer()), tensor.info()->total_size());
+        stream.read(reinterpret_cast<char *>(tensor.buffer() + tensor.info()->offset_first_element_in_bytes()), tensor.info()->total_size());
     }
     else
     {
