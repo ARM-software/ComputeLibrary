161b90c2ef1bd9a682efef8cfe3edec4c41898a2
Hakan Ardo <hakan@debian.org>
2018-02-15 21:02:30 +0100

    SubTensor's has to be accessed through execution windows

--- utils/GraphUtils.cpp
+++ utils/GraphUtils.cpp
@@ -24,6 +24,7 @@
 
 #include "utils/GraphUtils.h"
 #include "utils/Utils.h"
+#include "arm_compute/runtime/SubTensor.h"
 
 #ifdef ARM_COMPUTE_CL
 #include "arm_compute/core/CL/OpenCL.h"
@@ -206,7 +207,7 @@ void RandomAccessor::fill(ITensor &tensor, D &&distribution)
 {
     std::mt19937 gen(_seed);
 
-    if(tensor.info()->padding().empty())
+    if(tensor.info()->padding().empty() && !dynamic_cast<SubTensor*>(&tensor))
     {
         for(size_t offset = 0; offset < tensor.info()->total_size(); offset += tensor.info()->element_size())
         {
@@ -349,11 +350,10 @@ bool NumPyBinLoader::access_tensor(ITensor &tensor)
     }
 
     // Read data
-    if(tensor.info()->padding().empty())
+    if(tensor.info()->padding().empty() && !dynamic_cast<SubTensor*>(&tensor))
     {
         // If tensor has no padding read directly from stream.
-        size_t offset = tensor.info()->offset_first_element_in_bytes();
-        stream.read(reinterpret_cast<char *>(tensor.buffer() + offset), tensor.info()->total_size() - offset);
+        stream.read(reinterpret_cast<char *>(tensor.buffer()), tensor.info()->total_size());
     }
     else
     {
