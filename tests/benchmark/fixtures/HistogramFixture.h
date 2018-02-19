3ab6804a0e30be4d8591c8c84ae6a73940d0f2e2
John Richardson <john.richardson@arm.com>
2018-02-19 16:44:28 +0000

    COMPMID-761: Add CL/NEON Histogram benchmark tests
    Change-Id: Ie77bbce265ace735a9d8ec9db5bad70c74800507
    Reviewed-on: https://eu-gerrit-1.euhpc.arm.com/128035
    Reviewed-by: Georgios Pinitas <georgios.pinitas@arm.com>
    Tested-by: Jenkins <bsgcomp@arm.com>
    Reviewed-by: Anthony Barbier <anthony.barbier@arm.com>

---
+++ tests/benchmark/fixtures/HistogramFixture.h
@@ -0,0 +1,79 @@
+/*
+ * Copyright (c) 2018 ARM Limited.
+ *
+ * SPDX-License-Identifier: MIT
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to
+ * deal in the Software without restriction, including without limitation the
+ * rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+ * sell copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+#ifndef ARM_COMPUTE_TEST_HISTOGRAM_FIXTURE
+#define ARM_COMPUTE_TEST_HISTOGRAM_FIXTURE
+
+#include "arm_compute/core/TensorShape.h"
+#include "arm_compute/core/Types.h"
+#include "tests/Globals.h"
+#include "tests/Utils.h"
+#include "tests/framework/Fixture.h"
+
+namespace arm_compute
+{
+namespace test
+{
+namespace benchmark
+{
+template <typename TensorType, typename Function, typename Accessor, typename DistributionType>
+class HistogramFixture : public framework::Fixture
+{
+public:
+    template <typename...>
+    void setup(const TensorShape &shape, Format format)
+    {
+        // Create tensors
+        src = create_tensor<TensorType>(shape, format);
+
+        // Create and configure function
+        histogram_func.configure(&src, &dst);
+
+        // Allocate tensors
+        src.allocator()->allocate();
+    }
+
+    void run()
+    {
+        histogram_func.run();
+    }
+
+    void sync()
+    {
+        sync_if_necessary<TensorType>();
+    }
+
+    void teardown()
+    {
+        src.allocator()->free();
+    }
+
+private:
+    TensorType       src{};
+    DistributionType dst{ 30, 125, 255 };
+    Function         histogram_func{};
+};
+} // namespace benchmark
+} // namespace test
+} // namespace arm_compute
+#endif /* ARM_COMPUTE_TEST_HISTOGRAM_FIXTURE */
