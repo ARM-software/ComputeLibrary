2e01e18ff5458f4bd8e6bcc868ba7eb39ff776cc
Georgios Pinitas <georgios.pinitas@arm.com>
2018-06-06 14:35:15 +0100

    COMPMID-1246: Add GLES target in graph TypePrinter
    Change-Id: I5c6b29277b93facf6a992e02f72ebc6a1e5e5ef6
    Reviewed-on: https://eu-gerrit-1.euhpc.arm.com/134413
    Reviewed-by: Michele DiGiorgio <michele.digiorgio@arm.com>
    Tested-by: Jenkins <bsgcomp@arm.com>

--- arm_compute/graph/TypePrinter.h
+++ arm_compute/graph/TypePrinter.h
@@ -131,6 +131,9 @@ inline ::std::ostream &operator<<(::std::ostream &os, const Target &target)
         case Target::CL:
             os << "CL";
             break;
+        case Target::GC:
+            os << "GC";
+            break;
         default:
             ARM_COMPUTE_ERROR("NOT_SUPPORTED!");
     }
