861c2ec6502995d5ada21b06addd92cfeaa7a354
Giorgio Arena <giorgio.arena@arm.com>
2020-10-21 14:29:51 +0100

    COMPMID-3718 Remove OpenCL padding: CLFuseBatchNormalizationKernel
    Signed-off-by: Giorgio Arena <giorgio.arena@arm.com>
    Change-Id: I5f77356bff6c6ab513ed3555466c8c5bf5f4c4e3
    Reviewed-on: https://review.mlplatform.org/c/ml/ComputeLibrary/+/4227
    Reviewed-by: Michele Di Giorgio <michele.digiorgio@arm.com>
    Comments-Addressed: Arm Jenkins <bsgcomp@arm.com>
    Tested-by: Arm Jenkins <bsgcomp@arm.com>

--- src/core/CL/kernels/CLFuseBatchNormalizationKernel.cpp
+++ src/core/CL/kernels/CLFuseBatchNormalizationKernel.cpp
@@ -120,6 +120,8 @@ void CLFuseBatchNormalizationKernel::configure(const CLCompileContext &compile_c
 {
     ARM_COMPUTE_ERROR_ON_NULLPTR(input_weights, bn_mean, bn_var);
 
+    auto padding_info = get_padding_info({ input_weights, bn_mean, bn_var, fused_weights, fused_bias, input_bias, bn_beta, bn_gamma });
+
     _input_weights = input_weights;
     _input_bias    = input_bias;
     _bn_mean       = bn_mean;
@@ -172,6 +174,8 @@ void CLFuseBatchNormalizationKernel::configure(const CLCompileContext &compile_c
 
     // Create kernel
     _kernel = create_kernel(compile_context, "fuse_batchnormalization_layer", build_opts.options());
+
+    ARM_COMPUTE_ERROR_ON(has_padding_changed(padding_info));
 }
 
 Status CLFuseBatchNormalizationKernel::validate(const ITensorInfo *input_weights, const ITensorInfo *bn_mean, const ITensorInfo *bn_var,
