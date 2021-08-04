ced49dc53189c10daae2726bf49e8fcd2f09bf71
Freddie Liardet <frederick.liardet@arm.com>
2021-08-04 12:31:09 +0100

    Remove TODO, FIXME and COMPMID comments where possible
    Signed-off-by: Freddie Liardet <frederick.liardet@arm.com>
    Change-Id: I968787603927bcfbeacb110570eb488061ee3e43
    Reviewed-on: https://review.mlplatform.org/c/ml/ComputeLibrary/+/6058
    Tested-by: Arm Jenkins <bsgcomp@arm.com>
    Reviewed-by: Gunes Bayir <gunes.bayir@arm.com>
    Comments-Addressed: Arm Jenkins <bsgcomp@arm.com>

--- arm_compute/core/WindowIterator.h
+++ arm_compute/core/WindowIterator.h
@@ -28,9 +28,6 @@
 #include "arm_compute/core/ITensor.h"
 #include "arm_compute/core/Window.h"
 
-//FIXME: Delete the "ARM_COMPUTE_PRINTF" before the release. In the meantime it's probably going to be useful to debug
-//#define ARM_COMPUTE_PRINTF printf
-#define ARM_COMPUTE_PRINTF(...)
 
 namespace arm_compute
 {
@@ -170,14 +167,12 @@ public:
     {
         while(_end.z() != _position.z())
         {
-            ARM_COMPUTE_PRINTF("New slice %d\n", _position.z());
             iterate_2D_internal(on_new_row_size, _w.x().end() - _w.x().step(), _w.y().end() - _w.y().step());
             _position[2] += _w.z().step();
             _position[1] = _w.y().start();
             _position[0] = _w.x().start();
         }
         // Left over:
-        ARM_COMPUTE_PRINTF("Left over slice\n");
         iterate_2D(on_new_row_size);
     }
 
@@ -219,8 +214,6 @@ private:
         //Is there more than one row to process ?
         if(end_y == _position.y())
         {
-            // Single row:
-            ARM_COMPUTE_PRINTF("Partial row only\n");
             // Both start and end belong to the same row:
             iterate_over_dim0(end_x + _w.x().step(), on_new_row_size);
         }
@@ -230,7 +223,6 @@ private:
             if(_w.x().start() != _position.x())
             {
                 //Start in the middle of a row: process left-over X
-                ARM_COMPUTE_PRINTF("Partial row first\n");
                 iterate_over_dim0(_w.x().end(), on_new_row_size);
                 _position[1] += _w.y().step();
             }
@@ -239,7 +231,6 @@ private:
             bool no_leftover = end_x + _w.x().step() == _w.x().end();
             if(no_leftover)
             {
-                ARM_COMPUTE_PRINTF("no left over\n");
                 //Switch to full row size:
                 on_new_row_size(_w[0].start(), _w.x().end());
                 // Shouldn't be possible to reach that point and not have at least one entire row to process
@@ -249,17 +240,14 @@ private:
             }
             else
             {
-                ARM_COMPUTE_PRINTF("with left over\n");
                 // Are there full rows to process ?
                 if(_position[1] != end_y)
                 {
-                    ARM_COMPUTE_PRINTF("full rows\n");
                     //Switch to full row size:
                     on_new_row_size(_w[0].start(), _w.x().end());
                     iterate_over_dim1(end_y);
                 }
 
-                ARM_COMPUTE_PRINTF("Final leftover\n");
                 //Leftover end x
                 _position[0] = _w.x().start();
                 iterate_over_dim0(end_x + _w.x().step(), on_new_row_size);
@@ -298,7 +286,6 @@ private:
      */
     void iterate_over_dim0(int end)
     {
-        ARM_COMPUTE_PRINTF("X [%d, %d, %d]\n", _position.x(), end, _w[0].step());
         // Both start and end belong to the same row:
         ARM_COMPUTE_ERROR_ON(_position[0] > end);
         for(; _position.x() < end; _position[0] += _w[0].step())
