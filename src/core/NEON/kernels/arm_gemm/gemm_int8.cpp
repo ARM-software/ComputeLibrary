e42a87ffd23a334b802b47a52ec28ad6c90bfbf0
Gunes Bayir <gunes.bayir@arm.com>
2021-09-13 13:24:38 +0100

    Update cpu heuristics for GEMM
    If available, it prefers MMLA over DOT for smallK kernels
    for int8 types for faster execution.
    
    Signed-off-by: Gunes Bayir <gunes.bayir@arm.com>
    Change-Id: I190142dc66555476b4d8266d22e54ce8b45fffdb
    Reviewed-on: https://review.mlplatform.org/c/ml/ComputeLibrary/+/6251
    Tested-by: Arm Jenkins <bsgcomp@arm.com>
    Reviewed-by: Gian Marco Iodice <gianmarco.iodice@arm.com>
    Comments-Addressed: Arm Jenkins <bsgcomp@arm.com>

--- src/core/NEON/kernels/arm_gemm/gemm_int8.cpp
+++ src/core/NEON/kernels/arm_gemm/gemm_int8.cpp
@@ -67,7 +67,7 @@ GemmImplementation<int8_t, int32_t>::with_estimate(
     GemmMethod::GEMM_HYBRID,
     "sve_smallK_hybrid_s8s32_dot_8x1VL",
     [](const GemmArgs &args) { return args._ci->has_svei8mm() && args._Ksize<=64 && !args._indirect_input; },
-    nullptr,
+    [](const GemmArgs &args) { return !(args._ci->has_svei8mm() || args._ci->has_i8mm()); },
     [](const GemmArgs &args) { return new GemmHybrid<cls_sve_smallK_hybrid_s8s32_dot_8x1VL, int8_t, int32_t>(args); }
 },
 GemmImplementation<int8_t, int32_t>::with_estimate(
@@ -103,14 +103,14 @@ GemmImplementation<int8_t, int32_t>::with_estimate(
     GemmMethod::GEMM_HYBRID,
     "a64_smallK_hybrid_s8s32_dot_8x4",
     [](const GemmArgs &args) { return args._ci->has_dotprod() && (args._Nsize % 4 == 0) && (args._Ksize<=32) && !args._indirect_input; },
-    nullptr,
+    [](const GemmArgs &args) { return !(args._ci->has_svei8mm() || args._ci->has_i8mm()); },
     [](const GemmArgs &args) { return new GemmHybrid<cls_a64_smallK_hybrid_s8s32_dot_8x4, int8_t, int32_t>(args); }
 },
 {
     GemmMethod::GEMM_HYBRID,
     "a64_smallK_hybrid_s8s32_dot_6x4",
     [](const GemmArgs &args) { return args._ci->has_dotprod() && (args._Nsize % 4 == 0) && (args._Ksize>32) && (args._Ksize<=64) && !args._indirect_input; },
-    nullptr,
+    [](const GemmArgs &args) { return !(args._ci->has_svei8mm() || args._ci->has_i8mm()); },
     [](const GemmArgs &args) { return new GemmHybrid<cls_a64_smallK_hybrid_s8s32_dot_6x4, int8_t, int32_t>(args); }
 },
 {
