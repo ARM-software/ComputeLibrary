84a0941cf5bdaffc6127d4cae2e949e6e9109e4a
Pablo Marquez Tello <pablo.tello@arm.com>
2022-06-30 17:00:09 +0100

    Fix OpenBSD build errors
    * Partially resolves COMPMID-5396
    
    Change-Id: I7f3e692e73da548a901a07ed6d7212df40b8c26f
    Signed-off-by: Pablo Marquez Tello <pablo.tello@arm.com>
    Reviewed-on: https://review.mlplatform.org/c/ml/ComputeLibrary/+/7870
    Reviewed-by: Gunes Bayir <gunes.bayir@arm.com>
    Comments-Addressed: Arm Jenkins <bsgcomp@arm.com>
    Tested-by: Arm Jenkins <bsgcomp@arm.com>
    Benchmark: Arm Jenkins <bsgcomp@arm.com>

--- src/core/NEON/kernels/arm_conv/pooling/pooling_depthfirst.hpp
+++ src/core/NEON/kernels/arm_conv/pooling/pooling_depthfirst.hpp
@@ -27,8 +27,9 @@
 #include "depthfirst_driver.hpp"
 #include "src/core/NEON/kernels/arm_conv/addressing.hpp"
 #include "utils.hpp"
-
+#if !defined(_WIN64) && !defined(__OpenBSD__)
 #include <alloca.h>
+#endif /* !defined(_WIN64) && !defined(__OpenBSD__) */
 #include <limits>
 
 namespace arm_conv {
--- src/core/NEON/kernels/arm_conv/pooling/pooling_depthfirst_generic.hpp
+++ src/core/NEON/kernels/arm_conv/pooling/pooling_depthfirst_generic.hpp
@@ -27,7 +27,9 @@
 #include "arm_compute/core/Error.h"
 #include "depthfirst_driver.hpp"
 #include "utils.hpp"
+#if !defined(_WIN64) && !defined(__OpenBSD__)
 #include <alloca.h>
+#endif /* !defined(_WIN64) && !defined(__OpenBSD__) */
 
 namespace arm_conv {
 namespace pooling {
