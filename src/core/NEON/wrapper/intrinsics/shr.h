605a928960c0c36384925a9064d12addc8f43a41
Omar Al Khatib <omar.alkhatib@arm.com>
2022-11-01 17:01:24 +0000

    Optimize CPU mul layer on quantized data
    Resolves : [COMPMID-5461]
    
    Signed-off-by: Omar Al Khatib <omar.alkhatib@arm.com>
    Change-Id: I89b99d267c32b00ef44f9bb6e7c714dfe4a0d29d
    Reviewed-on: https://review.mlplatform.org/c/ml/ComputeLibrary/+/8420
    Comments-Addressed: Arm Jenkins <bsgcomp@arm.com>
    Tested-by: Arm Jenkins <bsgcomp@arm.com>
    Reviewed-by: Gunes Bayir <gunes.bayir@arm.com>
    Benchmark: Arm Jenkins <bsgcomp@arm.com>

--- src/core/NEON/wrapper/intrinsics/shr.h
+++ src/core/NEON/wrapper/intrinsics/shr.h
@@ -25,21 +25,19 @@
 #ifndef ARM_COMPUTE_WRAPPER_SHR_H
 #define ARM_COMPUTE_WRAPPER_SHR_H
 
-#include <type_traits>
 #include <arm_neon.h>
+#include <type_traits>
 
 namespace arm_compute
 {
 namespace wrapper
 {
-
 #define VQRSHRN_IMPL(half_vtype, vtype, prefix, postfix) \
     template <int b>                                     \
     inline half_vtype vqrshrn(const vtype &a)            \
     {                                                    \
         return prefix##_##postfix(a, b);                 \
     }
-
 VQRSHRN_IMPL(int8x8_t, int16x8_t, vqrshrn_n, s16)
 VQRSHRN_IMPL(uint8x8_t, uint16x8_t, vqrshrn_n, u16)
 VQRSHRN_IMPL(int16x4_t, int32x4_t, vqrshrn_n, s32)
@@ -77,19 +75,37 @@ VQRSHRN_SCALAR_IMPL(uint32_t, uint64_t, vqrshrnd_n, u64)
     {                                                                                                            \
         return prefix_signed##_##postfix(a, b);                                                                  \
     }                                                                                                            \
-                                                                                                                 \
+    \
     template <int b, typename T>                                                                                 \
     inline typename std::enable_if<std::is_integral<T>::value && !std::is_signed<T>::value, u##half_vtype>::type \
     vqrshrn_ex(const vtype &a)                                                                                   \
     {                                                                                                            \
         return prefix_unsigned##_##postfix(a, b);                                                                \
     }
-
 VQRSHRN_EX_IMPL(int8x8_t, int16x8_t, vqrshrn_n, vqrshrun_n, s16)
 VQRSHRN_EX_IMPL(int16x4_t, int32x4_t, vqrshrn_n, vqrshrun_n, s32)
 VQRSHRN_EX_IMPL(int32x2_t, int64x2_t, vqrshrn_n, vqrshrun_n, s64)
-
 #undef VQRSHRN_EX_IMPL
+
+#define VSHR_IMPL(vtype, prefix, postfix) \
+    template <int b>                      \
+    inline vtype vshr_n(const vtype &a)   \
+    {                                     \
+        return prefix##_##postfix(a, b);  \
+    }
+VSHR_IMPL(uint8x8_t, vshr_n, u8)
+VSHR_IMPL(int8x8_t, vshr_n, s8)
+#undef VSHR_IMPL
+
+#define VSHRQ_IMPL(vtype, prefix, postfix) \
+    template <int b>                       \
+    inline vtype vshrq_n(const vtype &a)   \
+    {                                      \
+        return prefix##_##postfix(a, b);   \
+    }
+VSHRQ_IMPL(uint32x4_t, vshrq_n, u32)
+VSHRQ_IMPL(int32x4_t, vshrq_n, s32)
+#undef VSHRQ_IMPL
 
 #ifdef __aarch64__
 #define VQRSHRN_EX_SCALAR_IMPL(half_vtype, vtype, prefix_signed, prefix_unsigned, postfix)                       \
