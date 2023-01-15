9476bc0e0fcd27bf63a53416839673f8bb69ef60
maxfx <maficccc@gmail.com>
2023-01-15 15:10:01 +0100

    Fix for FreeBSD 'OpenCLTimestamps'

--- tests/framework/Framework.cpp
+++ tests/framework/Framework.cpp
@@ -26,6 +26,8 @@
 #include "arm_compute/runtime/Scheduler.h"
 #include "tests/framework/ParametersLibrary.h"
 #include "tests/framework/TestFilter.h"
+#include "tests/framework/instruments/OpenCLTimer.h"
+#include "tests/framework/instruments/OpenCLMemoryUsage.h"
 
 #ifdef ARM_COMPUTE_CL
 #include "arm_compute/runtime/CL/CLRuntimeContext.h"
