af15076bf8148870aa41d414b73e81f3b7e86436
Ramy Elgammal <ramy.elgammal@arm.com>
2023-04-25 17:19:27 +0100

    Only define validation test tolerance for quantized types in case of aarch64 for Neon™ Matmul
    Partially Resovles: COMPMID-6026
    Signed-off-by: Ramy Elgammal <ramy.elgammal@arm.com>
    Change-Id: I273b213abba275f1609eae33058e3acbee2a7146
    Reviewed-on: https://review.mlplatform.org/c/ml/ComputeLibrary/+/9489
    Benchmark: Arm Jenkins <bsgcomp@arm.com>
    Tested-by: Arm Jenkins <bsgcomp@arm.com>
    Reviewed-by: Viet-Hoa Do <viet-hoa.do@arm.com>
    Comments-Addressed: Arm Jenkins <bsgcomp@arm.com>

--- tests/validation/NEON/MatMul.cpp
+++ tests/validation/NEON/MatMul.cpp
@@ -45,8 +45,10 @@ TEST_SUITE(MatMul)
 
 constexpr AbsoluteTolerance<float>   tolerance_fp32(0.001f); /**< Tolerance value for comparing reference's output against implementation's output for FP32 data types */
 const AbsoluteTolerance<half>        tolerance_fp16(half(0.1f));
+#ifdef __aarch64__
 constexpr AbsoluteTolerance<uint8_t> tolerance_qasymm8(0);
 constexpr AbsoluteTolerance<uint8_t> tolerance_qasymm8_signed(0);
+#endif // __aarch64__
 
 // clang-format off
 // *INDENT-OFF*
