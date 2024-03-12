c966f6f5139e4dae378a23ab3a80ec299ad25b36
hang xu <hangxu@hangs-MacBook-Pro.local>
2024-03-12 21:22:13 +0100

    Preprocess

--- utils/GraphUtils.cpp
+++ utils/GraphUtils.cpp
@@ -184,17 +184,19 @@ void WordPiecePreprocessor::preprocess_typed(ITensor &tensor,Args &&... tokens)
     //const T * pad_token     = reinterpret_cast<const T *>(get_nth_elm<0>(tokens...));
     const T * start_token   = reinterpret_cast<const T *>(get_nth_elm<1>(tokens...));
     const T * end_token     = reinterpret_cast<const T *>(get_nth_elm<2>(tokens...));
-    const T divide_helper   = reinterpret_cast<const T >(get_nth_elm<3>(tokens...));
+    const T * divide_helper   = reinterpret_cast<const T >(get_nth_elm<3>(tokens...));
 
     std::basic_string<T> buffer;
     buffer.append(start_token);
-    buffer+=divide_helper;
+    buffer.append(divide_helper);
+
     execute_window_loop(window,
                         [&](const Coordinates id){
                             std::cout << *reinterpret_cast<T *>(tensor.ptr_to_element(id));
                             buffer+= *reinterpret_cast<T *>(tensor.ptr_to_element(id));
                         });
-    buffer+=divide_helper;
+
+    buffer.append(divide_helper);
     buffer.append(end_token);
     std::cout << buffer;
 }
