f9c56c7ca2c4c728e52a6aab02dc2e080c6e2bf0
hang xu <hangxu@hangs-MacBook-Pro.local>
2024-03-12 22:34:55 +0100

    Preprocess

--- utils/GraphUtils.cpp
+++ utils/GraphUtils.cpp
@@ -206,7 +206,7 @@ void WordPiecePreprocessor::preprocess_typed(ITensor &tensor,Args &&... tokens)
     execute_window_loop(window,
                         [&](const Coordinates id){
                             i++;
-                            std::cout <<id[0]<<std::endl;
+                            std::cout <<buffer[id[0]]<<std::endl;
                             *reinterpret_cast<T *>(tensor.ptr_to_element(id)) = buffer[id[0]]; //Using dimesion x
                             std::cout << *reinterpret_cast<T *>(tensor.ptr_to_element(id));
                         });
