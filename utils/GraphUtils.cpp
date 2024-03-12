724c557c1b144da9c7d2ade468b16f5bd51724f4
hang xu <hangxu@re-byods-146-50-217-87.wireless.uva.nl>
2024-03-12 10:53:31 +0100

    Preprocessor

--- utils/GraphUtils.cpp
+++ utils/GraphUtils.cpp
@@ -137,9 +137,9 @@ void WordPiecePreprocessor::preprocess_typed(ITensor &tensor)
     std::cout << "data type ";
     std::cout << tensor.info()->data_type() << std::endl;
 
-    const T pad_token[6]   = "[PAD]";
-    const T start_token[6] = "[CLS]";
-    const T end_token[6]   = "[SEP]";
+    const T pad_token[6]   = reinterpret_cast<T*>("[PAD]");
+    const T start_token[6] = reinterpret_cast<T*>("[CLS]");
+    const T end_token[6]   = reinterpret_cast<T*>("[SEP]");
 
     //T* const buffer = pad_token;
 
