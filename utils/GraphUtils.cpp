5351bf025155420680d63d67fb1d4313628b453b
hang xu <hangxu@re-byods-146-50-217-87.wireless.uva.nl>
2024-03-12 17:10:11 +0100

    Preprocess

--- utils/GraphUtils.cpp
+++ utils/GraphUtils.cpp
@@ -151,7 +151,7 @@ void WordPiecePreprocessor::preprocess_typed(ITensor &tensor,Args &&... tokens)
     execute_window_loop(window,
                         [&](const Coordinates id){
                             std::cout << tensor.ptr_to_element(id);
-                            std::cout << * token;
+                            std::cout << *(token+1);
                         });
 }
 
