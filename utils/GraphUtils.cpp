d703034b14ab5b9dcbb4ecb95eb8ea95cdb77f19
hang xu <hangxu@re-byods-146-50-217-87.wireless.uva.nl>
2024-03-12 11:56:56 +0100

    Preprocessor

--- utils/GraphUtils.cpp
+++ utils/GraphUtils.cpp
@@ -111,8 +111,8 @@ void WordPiecePreprocessor::preprocess(ITensor &tensor)
     if (tensor.info()->data_type() == DataType::F32)
     {
         const char32_t pad_token[] =   U"[PAD]";
-        const char32_t start_token[] = U"[CLS]";
-        const char32_t end_token[] =   U"[SEP]";
+        //const char32_t start_token[] = U"[CLS]";
+        //const char32_t end_token[] =   U"[SEP]";
         preprocess_typed<char32_t,const char32_t*>(tensor,std::move(pad_token));
     }
     else if (tensor.info()->data_type() == DataType::F16)
