8a20cfa9a3668071eb1bed57054a7b84d6cdb9e4
hang xu <hangxu@hangs-MacBook-Pro.local>
2024-03-12 14:13:03 +0100

    Preprocess

--- utils/GraphUtils.cpp
+++ utils/GraphUtils.cpp
@@ -147,7 +147,7 @@ void WordPiecePreprocessor::preprocess_typed(ITensor &tensor,Args &&... tokens)
     execute_window_loop(window,
                         [&](const Coordinates id){
                             std::cout << tensor.ptr_to_element(id);
-                            std::cout << token.size();
+                            std::cout << std::get<0>(token);
                         });
 }
 
