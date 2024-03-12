a80f46cc8f325048e3f311d767ba4f0efc5889db
hang xu <hangxu@re-byods-146-50-217-87.wireless.uva.nl>
2024-03-12 17:58:36 +0100

    Preprocess

--- utils/GraphUtils.cpp
+++ utils/GraphUtils.cpp
@@ -108,23 +108,40 @@ WordPiecePreprocessor::WordPiecePreprocessor()
 
 void WordPiecePreprocessor::preprocess(ITensor &tensor)
 {
+    /* Currently only support UTF-8 */
     if (tensor.info()->data_type() == DataType::F32)
     {
+        const char pad_token[]          = u8"[PAD]";
+        const char start_token[]        = u8"[CLS]";
+        const char end_token[]          = u8"[SEP]";
+        preprocess_typed<char,const char *,const char *>(tensor,std::move(pad_token),
+                                                                std::move(start_token),
+                                                                std::move(end_token));
+        /*
         const char32_t pad_token[]      = U"[PAD]";
         const char32_t start_token[]    = U"[CLS]";
         const char32_t end_token[]      = U"[SEP]";
         preprocess_typed<char32_t,const char32_t *,const char32_t *>(tensor,std::move(pad_token),
                                                                             std::move(start_token),
                                                                             std::move(end_token));
+                                                                            */
     }
     else if (tensor.info()->data_type() == DataType::F16)
     {
+        const char pad_token[]          = u8"[PAD]";
+        const char start_token[]        = u8"[CLS]";
+        const char end_token[]          = u8"[SEP]";
+        preprocess_typed<char,const char *,const char *>(tensor,std::move(pad_token),
+                                                                std::move(start_token),
+                                                                std::move(end_token));
+        /*
         const char16_t pad_token[]      = u"[PAD]";
         const char16_t start_token[]    = u"[CLS]";
         const char16_t end_token[]      = u"[SEP]";
         preprocess_typed<char16_t,const char16_t *,const char16_t *>(tensor,std::move(pad_token),
                                                                             std::move(start_token),
                                                                             std::move(end_token));
+                                                                            */
     }
     else if (tensor.info()->data_type() == DataType::U8)
     {
