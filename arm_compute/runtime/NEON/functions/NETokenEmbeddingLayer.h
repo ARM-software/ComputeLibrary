3e9f7c308e64554690f23ee6d8727c17331f8b7c
hang xu <hangxu@hangs-MacBook-Pro.local>
2024-03-14 16:13:33 +0100

    Token Embedding Layer

--- arm_compute/runtime/NEON/functions/NETokenEmbeddingLayer.h
+++ arm_compute/runtime/NEON/functions/NETokenEmbeddingLayer.h
@@ -11,25 +11,24 @@ namespace arm_compute
 // Forward declarations
 class ITensor;
 class ITensorInfo;
-class Status;
 
-/** Basic function to execute concatenate tensors along a given axis */
 class NETokenEmbeddingLayer : public IFunction
 {
 public:
-    /** Default constructor */
+    /** Default Constructor */
     NETokenEmbeddingLayer();
-    /** Destructor */
+    /** Default Destructor */
     ~NETokenEmbeddingLayer();
     /** Prevent instances of this class from being copied (As this class contains pointers) */
     NETokenEmbeddingLayer(const NETokenEmbeddingLayer &) = delete;
-    /** Default move constructor */
-    NETokenEmbeddingLayer(NETokenEmbeddingLayer &&);
     /** Prevent instances of this class from being copied (As this class contains pointers) */
     NETokenEmbeddingLayer &operator=(const NETokenEmbeddingLayer &) = delete;
-    /** Default move assignment operator */
-    NETokenEmbeddingLayer &operator=(NETokenEmbeddingLayer &&);
-    /** Initialise the kernel's inputs vector and output.
+
+
+    /** TODO: Move able ?*/
+
+    
+    /** Set the input and output tensor.
      *
      * Valid data layouts:
      * - All
@@ -37,41 +36,29 @@ public:
      * Valid data type configurations:
      * |src            |dst            |
      * |:--------------|:--------------|
-     * |QASYMM8        |QASYMM8        |
-     * |QASYMM8_SIGNED |QASYMM8_SIGNED |
-     * |F16            |F16            |
-     * |F32            |F32            |
+     * |TBA            |TBA            |
+     * |TBA            |TBA            |
      *
-     * @note Input and output tensor dimensions preconditions defer depending on the concatenation axis.
-     * @note Preconditions can be found respectively at @ref cpu::kernels::CpuConcatenateWidthKernel, @ref cpu::kernels::CpuConcatenateHeightKernel,
-     *       @ref cpu::kernels::CpuConcatenateDepthKernel and @ref cpu::kernels::CpuConcatenateBatchKernel.
-     *
-     * @param[in,out] inputs_vector The vectors containing all the tensors to concatenate. Data types supported: QASYMM8/QASYMM8_SIGNED/F16/F32.
-     * @param[out]    output        Output tensor. Data types supported: Same as @p input.
-     * @param[in]     axis          Concatenation axis. Supported underlying concatenation axis are 0, 1, 2 and 3.
+     * @param[in]  d_model  Model dimension. Data types supported: TBA
+     * @param[out] output   Output tensor, shape (seq_len,d_model). Data type supported: TBA
      */
-    void configure(std::vector<const ITensor *> inputs_vector, ITensor *output, size_t axis);
-    /** Static function to check if given info will lead to a valid configuration of @ref NEConcatenateLayer
+    void configure(ITensor *input, ITensor *output, TokenEmbeddingLayerInfo activation_info);
+    /** Static function to check if given info will lead to a valid configuration of @ref NETokenEmbeddingLayer
      *
-     * @note Input and output tensor dimensions preconditions defer depending on the concatenation axis.
-     * @note Preconditions can be found respectively at @ref cpu::kernels::CpuConcatenateWidthKernel, @ref cpu::kernels::CpuConcatenateHeightKernel,
-     *       @ref cpu::kernels::CpuConcatenateDepthKernel and @ref cpu::kernels::CpuConcatenateBatchKernel.
-     *
-     * @param[in] inputs_vector The vectors containing all the tensors info to concatenate. Data types supported: QASYMM8/QASYMM8_SIGNED/F16/F32.
-     * @param[in] output        Output tensor info. Data types supported: Same as @p input.
-     * @param[in] axis          Concatenation axis. Supported underlying concatenation axis are 0, 1, 2 and 3.
+     * @param[in] input  Source tensor info. Data types supported: TBA
+     * @param[in] alpha  Source alpha tensor info. Data types supported: same of @p input.
+     * @param[in] output Destination tensor info. Data type supported: same as @p input
      *
      * @return a status
      */
-    static Status
-    validate(const std::vector<const ITensorInfo *> &inputs_vector, const ITensorInfo *output, size_t axis);
+    static Status validate(const unsigned int d_model, ITensor *output);
 
     // Inherited methods overridden:
     void run() override;
-
 private:
     struct Impl;
     std::unique_ptr<Impl> _impl;
 };
 } // namespace arm_compute
+
 #endif /* ARM_COMPUTE_NETOKENEMBEDDINGLAYER_H */
--- src/runtime/NEON/functions/NETokenEmbeddingLayer.cpp
+++ src/runtime/NEON/functions/NETokenEmbeddingLayer.cpp
@@ -1,91 +1,29 @@
-/*
- * Copyright (c) 2018-2021 Arm Limited.
- *
- * SPDX-License-Identifier: MIT
- *
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to
- * deal in the Software without restriction, including without limitation the
- * rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
- * sell copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be included in all
- * copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
- * SOFTWARE.
- */
 #include "arm_compute/runtime/NEON/functions/NETokenEmbeddingLayer.h"
 
-#include "arm_compute/core/Error.h"
-#include "arm_compute/core/ITensor.h"
-#include "arm_compute/core/TensorInfo.h"
-#include "arm_compute/core/Types.h"
-#include "arm_compute/core/utils/misc/ShapeCalculator.h"
-#include "arm_compute/runtime/NEON/NEScheduler.h"
+#include "arm_compute/core/Validate.h"
 
-#include "src/core/helpers/AutoConfiguration.h"
-#include "src/cpu/operators/CpuConcatenate.h"
+#include "src/cpu/operators/CpuTokenEmbed.h"
+// operator to be added 
 
 namespace arm_compute
 {
-struct NETokenEmbeddingLayer::Impl
-{
-    std::vector<const ITensor *>         srcs{};
-    ITensor                             *dst{nullptr};
-    unsigned int                         num_inputs{0};
-    unsigned int                         axis{0};
-    std::unique_ptr<cpu::CpuConcatenate> op{nullptr};
-};
 
-NETokenEmbeddingLayer::NETokenEmbeddingLayer() : _impl(std::make_unique<Impl>())
+
+NETokenEmbeddingLayer::NETokenEmbeddingLayer()
 {
 }
-NETokenEmbeddingLayer::NETokenEmbeddingLayer(NETokenEmbeddingLayer &&)            = default;
-NETokenEmbeddingLayer &NETokenEmbeddingLayer::operator=(NETokenEmbeddingLayer &&) = default;
-NETokenEmbeddingLayer::~NETokenEmbeddingLayer()                                = default;
 
-void NETokenEmbeddingLayer::configure(std::vector<const ITensor *> inputs_vector, ITensor *output, size_t axis)
+NETokenEmbeddingLayer::~NETokenEmbeddingLayer() = default;
+
+void NETokenEmbeddingLayer::configure(ITensor *input, ITensor *output, TokenEmbeddingLayerInfo tkemb_info)
 {
-    ARM_COMPUTE_ERROR_ON(output == nullptr);
-
-    _impl->srcs       = inputs_vector;
-    _impl->dst        = output;
-    _impl->axis       = axis;
-    _impl->num_inputs = inputs_vector.size();
-    _impl->op         = std::make_unique<cpu::CpuConcatenate>();
-
-    std::vector<const ITensorInfo *> inputs_vector_info;
-    for (unsigned int i = 0; i < inputs_vector.size(); ++i)
-    {
-        ARM_COMPUTE_ERROR_ON_NULLPTR(inputs_vector.at(i));
-        inputs_vector_info.emplace_back(inputs_vector.at(i)->info());
-    }
-    _impl->op->configure(inputs_vector_info, _impl->dst->info(), axis);
+        std::cout << " NETokenEmbeddingLayer::configure!!!!!!!!!!!!!!!  " << std::endl;
 }
 
-Status NETokenEmbeddingLayer::validate(const std::vector<const ITensorInfo *> &inputs_vector,
-                                    const ITensorInfo                      *output,
-                                    size_t                                  axis)
-{
-    return cpu::CpuConcatenate::validate(inputs_vector, output, axis);
-}
 
 void NETokenEmbeddingLayer::run()
 {
-    ITensorPack pack;
-    for (unsigned i = 0; i < _impl->num_inputs; ++i)
-    {
-        pack.add_tensor(TensorType::ACL_SRC_VEC + i, _impl->srcs.at(i));
-    }
-    pack.add_tensor(TensorType::ACL_DST, _impl->dst);
-
-    _impl->op->run(pack);
+    std::cout << " NETokenEmbeddingLayer::run!!!!!!!!!!!!!!!  " << std::endl;
 }
+
 } // namespace arm_compute
