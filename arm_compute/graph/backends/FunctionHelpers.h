ee94a7560ff117f32d1b94d373f9ab153e13aeec
hang xu <hangxu@re-byods-146-50-207-22.wireless.uva.nl>
2024-03-14 13:17:07 +0100

    Token Embedding Layer

--- arm_compute/graph/backends/FunctionHelpers.h
+++ arm_compute/graph/backends/FunctionHelpers.h
@@ -1678,37 +1678,7 @@ std::unique_ptr<IFunction> create_strided_slice_layer(StridedSliceLayerNode &nod
     return func;
 }
 
-/** Creates a backend token embedding layer function
- *
- * @tparam TokenEmbeddingLayerFunction  Backend token embedding function
- * @tparam TargetInfo                   Target-specific information
- *
- * @param[in] node Node to create the backend function for
- *
- * @return Backend token embedding layer function
- */
-template <typename TokenEmbeddingLayerFunction, typename TargetInfo>
-std::unique_ptr<IFunction> create_token_embedding_layer(TokenEmbeddingLayerNode &node)
-{
 
-    std::cout << "create_token_embedding_layer(TokenEmbeddingLayerNode &node)" << std::endl;
-    validate_node<TargetInfo>(node, 1 /* expected inputs */, 1 /* expected outputs */);
-
-    // Extract IO and info
-    /*
-    typename TargetInfo::TensorType *input    = get_backing_tensor<TargetInfo>(node.input(0));
-    typename TargetInfo::TensorType *output   = get_backing_tensor<TargetInfo>(node.output(0));
-    const TokenEmbeddingLayerInfo tkemb_info  = node.token_embedding_info();
-    */
-    // Create function
-    auto func = std::make_unique<TokenEmbeddingLayerFunction>();
-
-    ARM_COMPUTE_LOG_GRAPH_INFO(
-        "Instantiated " << node.name() << " Type: " << node.type() << " Target: " << TargetInfo::TargetType
-                        << " Data Type: " << input->info()->data_type() << " Shape: " << input->info()->tensor_shape() << std::endl);
-
-    return func;
-}
 
 } // namespace detail
 } // namespace backends
