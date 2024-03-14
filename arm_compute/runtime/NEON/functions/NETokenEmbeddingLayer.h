f3443dfc7a4967f793237278c27ebf286caf0ba3
hang xu <hangxu@re-byods-146-50-207-22.wireless.uva.nl>
2024-03-14 13:00:11 +0100

    Token Embedding Layer

--- arm_compute/runtime/NEON/functions/NETokenEmbeddingLayer.h
+++ arm_compute/runtime/NEON/functions/NETokenEmbeddingLayer.h
@@ -3,7 +3,6 @@
 
 #include "arm_compute/core/Types.h"
 #include "arm_compute/runtime/IFunction.h"
-#include "arm_compute/runtime/IRuntimeContext.h"
 
 #include <memory>
 
@@ -17,7 +16,7 @@ class NETokenEmbeddingLayer : public IFunction
 {
 public:
     /** Default Constructor */
-    NETokenEmbeddingLayer(IRuntimeContext *ctx = nullptr);
+    NETokenEmbeddingLayer();
     /** Default Destructor */
     ~NETokenEmbeddingLayer();
     /** Prevent instances of this class from being copied (As this class contains pointers) */
@@ -25,42 +24,10 @@ public:
     /** Prevent instances of this class from being copied (As this class contains pointers) */
     NETokenEmbeddingLayer &operator=(const NETokenEmbeddingLayer &) = delete;
 
-
-    /** TODO: Move able ?*/
-
-    
-    /** Set the input and output tensor.
-     *
-     * Valid data layouts:
-     * - All
-     *
-     * Valid data type configurations:
-     * |src            |dst            |
-     * |:--------------|:--------------|
-     * |TBA            |TBA            |
-     * |TBA            |TBA            |
-     *
-     * @param[in, out] input           Source tensor. In case of @p output tensor = nullptr, this tensor will store the result
-     *                                 of the activation function. Data types supported: QASYMM8/QASYMM8_SIGNED/QSYMM16/F16/F32.
-     * @param[out]     output          Destination tensor. Data type supported: same as @p input
-     * @param[in]      activation_info Token embedding layer parameters.
-     */
-    void configure(ITensor *input, ITensor *output, TokenEmbeddingLayerInfo tkemb_info);
-    /** Static function to check if given info will lead to a valid configuration of @ref NETokenEmbeddingLayer
-     *
-     * @param[in] input  Source tensor info. Data types supported: TBA
-     * @param[in] alpha  Source alpha tensor info. Data types supported: same of @p input.
-     * @param[in] output Destination tensor info. Data type supported: same as @p input
-     *
-     * @return a status
-     */
-    static Status validate(const ITensorInfo *input, const ITensorInfo *output, const TokenEmbeddingLayerInfo &tkemb_info);
-
     // Inherited methods overridden:
     void run() override;
 private:
-    struct Impl;
-    std::unique_ptr<Impl> _impl;
+    
 };
 } // namespace arm_compute
 
--- src/runtime/NEON/functions/NETokenEmbeddingLayer.cpp
+++ src/runtime/NEON/functions/NETokenEmbeddingLayer.cpp
@@ -7,33 +7,12 @@
 
 namespace arm_compute
 {
-struct NETokenEmbeddingLayer::Impl
+    
+NETokenEmbeddingLayer::NETokenEmbeddingLayer()
 {
-    const ITensor                      *src{nullptr};
-    ITensor                            *dst{nullptr};
-    IRuntimeContext                    *ctx{nullptr};
-};
-
-
-NETokenEmbeddingLayer::NETokenEmbeddingLayer(IRuntimeContext *ctx) : _impl(std::make_unique<Impl>())
-{
-    _impl->ctx = ctx;
 }
 
 NETokenEmbeddingLayer::~NETokenEmbeddingLayer() = default;
-
-void NETokenEmbeddingLayer::configure(ITensor *input, ITensor *output, TokenEmbeddingLayerInfo tkemb_info)
-{
-        std::cout << " NETokenEmbeddingLayer::configure!!!!!!!!!!!!!!!  " << std::endl;
-}
-
-Status
-NETokenEmbeddingLayer::validate(const ITensorInfo *input, const ITensorInfo *output, const TokenEmbeddingLayerInfo &tkemb_info)
-{
-    std::cout << "NETokenEmbeddingLayer::validate" << std::endl;
-    std::cout << tkemb_info.d_model() << std::endl;
-    return cpu::CpuTokenEmbed::validate(input, output);
-}
 
 void NETokenEmbeddingLayer::run()
 {
