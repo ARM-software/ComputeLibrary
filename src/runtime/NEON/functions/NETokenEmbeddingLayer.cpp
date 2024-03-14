7d2ee4189642f2b0f19fcd9253d97fc8d6a16f1f
hang xu <hangxu@re-byods-146-50-207-22.wireless.uva.nl>
2024-03-14 11:24:47 +0100

    Token Embedding Layer

--- src/runtime/NEON/functions/NETokenEmbeddingLayer.cpp
+++ src/runtime/NEON/functions/NETokenEmbeddingLayer.cpp
@@ -6,13 +6,20 @@
 
 namespace arm_compute
 {
-    void NETokenEmbeddingLayer::configure(ITensor *input, ITensor *output, TokenEmbeddingLayerInfo tkemb_info)
-    {
-         std::cout << " NETokenEmbeddingLayer::configure!!!!!!!!!!!!!!!  " << std::endl;
-    }
 
-    void NETokenEmbeddingLayer::run()
-    {
-        std::cout << " NETokenEmbeddingLayer::run!!!!!!!!!!!!!!!  " << std::endl;
-    }
+NETokenEmbeddingLayer::NETokenEmbeddingLayer()
+{
+}
+NETokenEmbeddingLayer::~NETokenEmbeddingLayer() = default;
+
+void NETokenEmbeddingLayer::configure(ITensor *input, ITensor *output, TokenEmbeddingLayerInfo tkemb_info)
+{
+        std::cout << " NETokenEmbeddingLayer::configure!!!!!!!!!!!!!!!  " << std::endl;
+}
+
+void NETokenEmbeddingLayer::run()
+{
+    std::cout << " NETokenEmbeddingLayer::run!!!!!!!!!!!!!!!  " << std::endl;
+}
+
 } // namespace arm_compute
