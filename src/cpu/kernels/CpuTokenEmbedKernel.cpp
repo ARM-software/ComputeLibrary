edabe45a7243d769f1a5ac4afc6a523a58d0417c
hang xu <hangxu@hangs-MacBook-Pro.local>
2024-03-15 17:25:31 +0100

    Token Embedding Kernel

--- src/cpu/kernels/CpuTokenEmbedKernel.cpp
+++ src/cpu/kernels/CpuTokenEmbedKernel.cpp
@@ -1,7 +1,10 @@
 #include "src/cpu/kernels/CpuTokenEmbedKernel.h"
 
-#include "src/core/common/Registrars.h"
+#include "arm_compute/core/ITensor.h"
+#include "arm_compute/core/TensorInfo.h"
+#include "arm_compute/core/Utils.h"
 
+#include "src/core/common/Registrars.h"
 #include "src/cpu/kernels/tokenembed/list.h"
 
 namespace arm_compute
@@ -13,7 +16,6 @@ namespace kernels
 
 namespace
 {
-
 static const std::vector<CpuTokenEmbedKernel::TKEMBKernel> avaiable_kernels = {
     /*
 #ifdef ARM_COMPUTE_ENABLE_SVE
@@ -28,7 +30,14 @@ static const std::vector<CpuTokenEmbedKernel::TKEMBKernel> avaiable_kernels = {
      REGISTER_FP32_NEON(arm_compute::cpu::neon_token_embed_char_2_float32)}
 
 };
+}
 
+void CpuTokenEmbedKernel::configure(const ITensorInfo *src, ITensorInfo *dst, TokenEmbeddingLayerInfo tkemb_info)
+{
+    ARM_COMPUTE_UNUSED(dst);
+    ARM_COMPUTE_ERROR_ON_NULLPTR(src);
+    ARM_COMPUTE_ERROR_THROW_ON(validate_arguments(src, dst, activation_info));
+    
 }
 
 } // namespace kernels
