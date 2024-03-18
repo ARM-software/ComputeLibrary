bc5a8007e0b8a3230c64c1a7fb4e0a5c77a1897f
hang xu <hangxu@hangs-MacBook-Pro.local>
2024-03-18 16:48:10 +0100

    Token Embedding Kernel

--- utils/GraphUtils.cpp
+++ utils/GraphUtils.cpp
@@ -217,8 +217,8 @@ void WordPiecePreprocessor::preprocess_typed(ITensor &tensor,Args &&... tokens)
     
     /** Sepreate into tokens and look up vocab list */
     std::map<std::string,int> token2id = get_token2id(_vocab_file);
-    for (auto item:token2id)
-    std::cout << item << std::endl;
+    for (const auto& [key, value] : token2id)
+        std::cout << '[' << key << "] = " << value << "; ";
     /*
     std::vector<int> text_ids;
     int v_size = 0;
