19f7630e6c9f54cb74c3f2c02b2b7b424865ffec
hang xu <hangxu@re-byods-146-50-207-209.wireless.uva.nl>
2024-03-18 20:11:21 +0100

    Token Embedding Kernel

--- utils/GraphUtils.cpp
+++ utils/GraphUtils.cpp
@@ -230,7 +230,7 @@ void WordPiecePreprocessor::preprocess_typed(ITensor &tensor,Args &&... tokens)
     while(token != NULL)
     {
         std::cout << token << std::endl;
-        text_ids[v_size++] = token2id[token];
+        text_ids.push_back(token2id[token]);
         token = std::strtok(nullptr, " ");
     }
 
