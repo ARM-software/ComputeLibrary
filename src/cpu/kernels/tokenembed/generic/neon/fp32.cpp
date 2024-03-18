270f00876c53bc4ca584ff88f228e9c4ea7367fe
hang xu <hangxu@re-byods-146-50-236-190.wireless.uva.nl>
2024-03-18 10:55:32 +0100

    Token Embedding Kernel

--- src/cpu/kernels/tokenembed/generic/neon/fp32.cpp
+++ src/cpu/kernels/tokenembed/generic/neon/fp32.cpp
@@ -19,7 +19,7 @@ void neon_token_embed_char_2_float32(const ITensor *src, const ITensor *vocab, I
 
     Window win = window;
     //win.set(Window::DimX, Window::Dimension(0,tkemb_info.d_vocab(),1 ));
-    ..win.set(Window::DimY, Window::Dimension(0,tkemb_info.d_model(),1 ));
+    //win.set(Window::DimY, Window::Dimension(0,tkemb_info.d_model(),1 ));
     
     Iterator src_iter(src,win);
     Iterator vocab_iter(vocab,win);
