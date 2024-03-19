e3a6d5463ae2f9285fae6310de024357afc0d0e6
hang xu <hangxu@re-byods-146-50-209-218.wireless.uva.nl>
2024-03-19 10:18:04 +0100

    Token Embedding Kernel

--- arm_compute/graph/backends/FunctionHelpers.h
+++ arm_compute/graph/backends/FunctionHelpers.h
@@ -1695,12 +1695,13 @@ std::unique_ptr<IFunction> create_token_embedding_layer(TokenEmbeddingLayerNode 
     // Extract IO and info
     
     typename TargetInfo::TensorType *input    = get_backing_tensor<TargetInfo>(node.input(0));
+    typename TargetInfo::TensorType *vocab    = get_backing_tensor<TargetInfo>(node.input(1));
     typename TargetInfo::TensorType *output   = get_backing_tensor<TargetInfo>(node.output(0));
     const TokenEmbeddingLayerInfo tkemb_info  = node.token_embedding_info();
 
     // Create function
     auto func = std::make_unique<TokenEmbeddingLayerFunction>();
-    func->configure(input,output,tkemb_info);
+    func->configure(input,vocab,output,tkemb_info);
 
     ARM_COMPUTE_LOG_GRAPH_INFO(
         "Instantiated " << node.name() << " Type: " << node.type() << " Target: " << TargetInfo::TargetType
