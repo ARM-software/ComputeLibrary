24aa80251c89b881e4783f676384f25178a22cf4
hang xu <hangxu@re-byods-146-50-209-218.wireless.uva.nl>
2024-03-19 11:35:38 +0100

    Token Embedding Kernel

--- src/cpu/kernels/tokenembed/generic/neon/fp32.cpp
+++ src/cpu/kernels/tokenembed/generic/neon/fp32.cpp
@@ -11,16 +11,6 @@ void neon_token_embed_char_2_float32(const ITensor *src, const ITensor *vocab, I
 {
     std::cout << "src/cpu/kernels/tokenembed/generic/neon/fp32.cpp: neon_token_embed_char_2_float32" << std::endl;
 
-    std::cout << src->info()->id() << std::endl;
-    std::cout << vocab->info()->id() << std::endl;
-    std::cout << dst->info()->id() << std::endl;
-    std::cout << tkemb_info.d_vocab() << std::endl;
-    
-    std::cout << "Window" << std::endl;
-    std::cout << window.DimX << std::endl;
-    std::cout << window.DimY << std::endl;
-    std::cout << window.DimZ << std::endl;
-
     Window win = window;
     win.set(Window::DimX, Window::Dimension(0,1,1));
     const unsigned int window_start_x   = static_cast<unsigned int>(window.x().start());
@@ -28,8 +18,8 @@ void neon_token_embed_char_2_float32(const ITensor *src, const ITensor *vocab, I
     unsigned int       x                = window_start_x;
 
     std::cout << "Tensor shape" << std::endl;
-    std::cout << window_start_x << std::endl;
-    std::cout << window_end_x << std::endl;
+    std::cout << dst->info()->tensor_shape().x() << std::endl;
+    std::cout << dst->info()->tensor_shape().y() << std::endl;
     
     Iterator src_iter(src,win);
     Iterator vocab_iter(vocab,win);
