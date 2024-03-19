7b477951f51cf47d19375a867e2c4fe77933e5f5
hang xu <hangxu@re-byods-146-50-209-218.wireless.uva.nl>
2024-03-19 10:25:58 +0100

    Token Embedding Kernel

--- src/cpu/kernels/tokenembed/generic/neon/fp32.cpp
+++ src/cpu/kernels/tokenembed/generic/neon/fp32.cpp
@@ -12,6 +12,7 @@ void neon_token_embed_char_2_float32(const ITensor *src, const ITensor *vocab, I
     std::cout << "src/cpu/kernels/tokenembed/generic/neon/fp32.cpp: neon_token_embed_char_2_float32" << std::endl;
 
     std::cout << src->info()->id() << std::endl;
+    std::cout << vocab->info()->id() << std::endl;
     std::cout << dst->info()->id() << std::endl;
     std::cout << tkemb_info.d_vocab() << std::endl;
     std::cout << window.DimX << std::endl;
@@ -20,13 +21,16 @@ void neon_token_embed_char_2_float32(const ITensor *src, const ITensor *vocab, I
     win.set(Window::DimX, Window::Dimension(0,1,1 ));
     //win.set(Window::DimY, Window::Dimension(0,tkemb_info.d_model(),1 ));
     
+    Iterator src_iter(src,win);
     Iterator vocab_iter(vocab,win);
 
+    const auto src_ptr      = reinterpret_cast<char *>(src_iter.ptr());
     const auto vocab_ptr    = reinterpret_cast<float *>(vocab_iter.ptr());
 
     std::cout << "YeaHhhhhhhhhhhh " << std::endl;
     execute_window_loop(win,
         [&](const Coordinates &){
+            std::cout << *(src_ptr) << std::endl;
             std::cout << *(vocab_ptr) << std::endl;
         },vocab_iter);
 
