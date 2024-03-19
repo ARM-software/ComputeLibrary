3fa07e8bc1e6487c4aa51ca0d41eae9afee7b27b
hang xu <hangxu@re-byods-146-50-209-218.wireless.uva.nl>
2024-03-19 10:14:52 +0100

    Token Embedding Kernel

--- src/cpu/kernels/tokenembed/generic/neon/fp32.cpp
+++ src/cpu/kernels/tokenembed/generic/neon/fp32.cpp
@@ -12,7 +12,6 @@ void neon_token_embed_char_2_float32(const ITensor *src, const ITensor *vocab, I
     std::cout << "src/cpu/kernels/tokenembed/generic/neon/fp32.cpp: neon_token_embed_char_2_float32" << std::endl;
 
     std::cout << src->info()->id() << std::endl;
-    std::cout << vocab->info()->id() << std::endl;
     std::cout << dst->info()->id() << std::endl;
     std::cout << tkemb_info.d_vocab() << std::endl;
     std::cout << window.DimX << std::endl;
