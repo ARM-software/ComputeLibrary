835cdf743cb1d5cd49e9cc670f631379e4bc44bd
hang xu <hangxu@hangs-MacBook-Pro.local>
2024-03-20 13:14:10 +0100

    Token Embedding Kernel

--- src/graph/GraphBuilder.cpp
+++ src/graph/GraphBuilder.cpp
@@ -642,8 +642,8 @@ NodeID GraphBuilder::add_multi_head_attention_node(Graph &g, NodeParams params, 
     /* Value, Key, Query Linear Layers */
 
     /* Scale dot production Layer */
-    std::unique_ptr<ScaleDotProductionAttentionLayerInfo> sdpa_info = std::make_unique<ScaleDotProductionAttentionLayerInfo>(mha_info);
-    NodeID sdp_nid = g.add_node<ScaleDotProductionAttentionNode>(*sdpa_info);
+    //std::unique_ptr<ScaleDotProductionAttentionLayerInfo> sdpa_info = std::make_unique<ScaleDotProductionAttentionLayerInfo>(mha_info);
+    NodeID sdp_nid = g.add_node<ScaleDotProductionAttentionNode>(ScaleDotProductionAttentionLayerInfo(768U,12U));
     /* Concate */
 
     /* Linear */
