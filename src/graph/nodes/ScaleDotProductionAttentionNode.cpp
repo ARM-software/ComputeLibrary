01fd2330161303aa00f95b52ae4fecf1033c29b1
hang xu <hangxu@hangs-MacBook-Pro.local>
2024-03-20 16:18:18 +0100

    Token Embedding Kernel

--- src/graph/nodes/ScaleDotProductionAttentionNode.cpp
+++ src/graph/nodes/ScaleDotProductionAttentionNode.cpp
@@ -21,7 +21,7 @@ const ScaleDotProductionAttentionLayerInfo &ScaleDotProductionAttentionNode::sdp
 
 bool ScaleDotProductionAttentionNode::forward_descriptors()
 {
-    if ((input_id(0) != NullTensorID) && (output_id(0) != NullTensorID))
+    if ((input_id(0) != NullTensorID) && (input_id(1) != NullTensorID) && (output_id(0) != NullTensorID))
     {
         Tensor *dst = output(0);
         ARM_COMPUTE_ERROR_ON(dst == nullptr);
