2b1a799d6bbb64f0b656bd51940e59b90f74eaa0
hang xu <hangxu@re-byods-146-50-193-34.wireless.uva.nl>
2024-03-21 13:11:09 +0100

    Token Embedding Kernel

--- src/runtime/NEON/functions/NELinearLayer.cpp
+++ src/runtime/NEON/functions/NELinearLayer.cpp
@@ -28,9 +28,6 @@ void NELinearLayer::configure(const ITensor *input1, ITensor *output, const Line
 
     _impl->kernel = std::make_unique<cpu::CpuLinear>();
     _impl->kernel->configure(input1->info(), output->info(), linear_info);
-
-    std::cout << "src/runtime/NEON/functions/NELinearLayer.cpp" << std::endl;
-    std::cout << linear_info.d_model() << std::endl;
 }
 
 Status NELinearLayer::validate(const ITensorInfo *input1, const ITensorInfo *output, const LinearLayerInfo& linear_info)
@@ -43,6 +40,8 @@ void NELinearLayer::run()
     ITensorPack pack;
     pack.add_tensor(TensorType::ACL_SRC_0, _impl->src);
     pack.add_tensor(TensorType::ACL_DST, _impl->dst);
+    
+    std::cout << "src/runtime/NEON/functions/NELinearLayer.cpp run" << std::endl;
     _impl->kernel->run(pack);
 }
 
