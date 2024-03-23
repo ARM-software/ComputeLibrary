3be5b9d55b681c3d963c837d01c81d6144d04723
hang xu <hangxu@re-byods-146-50-141-132.wireless.uva.nl>
2024-03-23 11:17:29 +0100

    Token Embedding Kernel

--- src/graph/GraphBuilder.cpp
+++ src/graph/GraphBuilder.cpp
@@ -699,7 +699,7 @@ NodeID GraphBuilder::add_linear_layer(Graph &g, NodeParams params, NodeIdxPair i
     NodeID v_nid    = g.add_node<LinearLayerNode>(v_linear_info);
 
     // A node to hold all the output
-    //NodeID p_nid    = g.add_node<ParallelTensorHoldingNode>(1);
+    NodeID f_nid    = g.add_node<SimpleForwardLayerNode>(1);
     
     // Connect input
     //g.add_connection(input.node_id, input.index, q_nid, 0);
@@ -715,12 +715,12 @@ NodeID GraphBuilder::add_linear_layer(Graph &g, NodeParams params, NodeIdxPair i
     g.add_connection(v_b_nid, 0, v_nid, 2);
 
     // Connect all linear node to single node
-    //g.add_connection(v_nid, 0, p_nid,0);
+    g.add_connection(v_nid, 0, f_nid,0);
 
     //set_node_params(g, q_nid, params);
     //set_node_params(g, k_nid, params);
     set_node_params(g, v_nid, params);
-    //set_node_params(g, p_nid, params);
+    set_node_params(g, f_nid, params);
 
     return v_nid;
 }
