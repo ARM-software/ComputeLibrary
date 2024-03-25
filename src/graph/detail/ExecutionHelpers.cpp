f999ded4d28c733bf1306985077b5909180522e4
hang xu <hangxu@hangs-MacBook-Pro.local>
2024-03-25 13:04:05 +0100

    Positional Encoding

--- src/graph/detail/ExecutionHelpers.cpp
+++ src/graph/detail/ExecutionHelpers.cpp
@@ -199,11 +199,13 @@ void call_all_const_node_accessors(Graph &g)
     {
         if (node != nullptr && node->type() == NodeType::Const && node->num_outputs())
         {
-            for(auto idx : node->outputs())
+            for(size_t idx=0;idx <node->num_outputs(); idx++)
             {   
                 std::cout << idx << std::endl;
                 if (!node->output(idx)->bound_edges().empty())
-                call_tensor_accessor(node->output(idx));
+                {
+                    call_tensor_accessor(node->output(idx));
+                }
             }
             /** Original only call 1 output
              * if (!node->output(0)->bound_edges().empty())
