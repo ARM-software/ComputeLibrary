d73b3c5b4784aee4f8bdb3828361a8fdce570ab6
hang xu <hangxu@hangs-MacBook-Pro.local>
2024-03-26 11:12:07 +0100

    Positional Encoding

--- cp.sh
+++
@@ -1,4 +0,0 @@
-git pull
-scons Werror=1 debug=0 asserts=0 neon=1 opencl=1 examples=1 os=linux arch=armv8a -j4
-cp build/libarm_compute* /home/run/lib 
-cp build/examples/graph_vanilla_transformer /home/run 
