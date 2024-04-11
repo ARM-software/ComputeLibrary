330fc331a2f6b02577a98adc46b51b89b7a39b7d
dondavan <yeezushugh@gmail.com>
2024-04-11 15:41:26 +0200

    Multihead Attention

--- arm_compute/graph/backends/FunctionHelpers.h
+++ arm_compute/graph/backends/FunctionHelpers.h
@@ -1837,14 +1837,14 @@ std::unique_ptr<IFunction> create_scale_dot_production_layer(ScaleDotProductionA
 
      // Extract IO and info
     typename TargetInfo::TensorType *query   = get_backing_tensor<TargetInfo>(node.input(0));
-    std::cout << "arm_compute/graph/backends/FunctionHelpers.h" << std::endl;
-    std::cout <<"id " << node.id() << std::endl;
+    std::cout << "arm_compute/graph/backends/FunctionHelpers.h  "<< node.id() << "  Start" << std::endl;
     std::cout << node.input(0)->desc().shape.x() << std::endl;
     typename TargetInfo::TensorType *key     = get_backing_tensor<TargetInfo>(node.input(1));
     std::cout << node.input(1)->desc().shape.x() << std::endl;
     typename TargetInfo::TensorType *value   = get_backing_tensor<TargetInfo>(node.input(2));
     std::cout << node.input(2)->desc().shape.x() << std::endl;
     typename TargetInfo::TensorType *output  = get_backing_tensor<TargetInfo>(node.output(0));
+    std::cout << "arm_compute/graph/backends/FunctionHelpers.h  "<< node.id() << "  End" << std::endl;
 
     ARM_COMPUTE_ERROR_ON(input == nullptr);
     ARM_COMPUTE_ERROR_ON(output == nullptr);
