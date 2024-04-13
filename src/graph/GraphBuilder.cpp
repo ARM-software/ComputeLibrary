ec5a4bf762cddd3c60de0e66ac5dbb138548a28e
dondavan <yeezushugh@gmail.com>
2024-04-13 21:11:56 +0200

    Multihead Attention

--- src/graph/GraphBuilder.cpp
+++ src/graph/GraphBuilder.cpp
@@ -969,10 +969,14 @@ NodeID GraphBuilder::add_embedding_node(Graph &g,
     // Reshape tensor to store weight with size of vocabulary and depth of d_model.
     s_desc.shape = TensorShape(2 /* Segment 0,1 */,tkemb_info.d_model());
 
+    // Position const node output tensor descriptor
+    TensorDescriptor p_desc = input_tensor_desc;
+    // Reshape tensor to store weight with size of vocabulary and depth of d_model.
+    p_desc.shape = TensorShape(512 /* Pretrained */,tkemb_info.d_model());
 
     NodeID v_c_nid  = add_const_node_with_name(g, params, "vocabs", v_desc,    std::move(vocabs_accessor));
     NodeID s_c_nid  = add_const_node_with_name(g, params, "segements", s_desc, std::move(segemnts_accessor));
-    NodeID p_c_nid  = add_const_node_with_name(g, params, "position", s_desc, std::move(position_accessor));
+    NodeID p_c_nid  = add_const_node_with_name(g, params, "position", p_desc, std::move(position_accessor));
 
     // Create token embedding node and connect
     NodeID t_nid = g.add_node<TokenEmbeddingLayerNode>(tkemb_info);
