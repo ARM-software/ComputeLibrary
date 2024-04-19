24927dc2c2841413531aaf7258454bf52c9f9e53
dondavan <yeezushugh@gmail.com>
2024-04-19 14:00:55 +0200

    Verifying tensor

--- src/cpu/kernels/CpuVectorizeKernel.cpp
+++ src/cpu/kernels/CpuVectorizeKernel.cpp
@@ -112,18 +112,6 @@ void CpuVectorizeKernel::run_op(ITensorPack &tensors, const Window &window, cons
     const ITensor *vector   = tensors.get_const_tensor(TensorType::ACL_SRC_1);
     ITensor       *dst      = tensors.get_tensor(TensorType::ACL_DST);
 
-
-    std::cout << "src/cpu/kernels/CpuTokenEmbedKernel.cpp reshaped" << std::endl;
-
-    size_t reshape_input_x = src->info()->valid_region().shape.x();
-    /* Runtime input reshape */
-    if(src->info()->tensor_shape().x() != reshape_input_x)
-    {
-        dst->info()->set_valid_region(dst->info()->valid_region().set(0,0,reshape_input_x));
-        std::cout << "dst: "<< dst->info()->valid_region().shape.x() << std::endl;
-
-    }
-
     _run_method(src, vector, dst, window);
 }
 
--- src/cpu/kernels/vectorize/generic/neon/fp32.cpp
+++ src/cpu/kernels/vectorize/generic/neon/fp32.cpp
@@ -2,6 +2,7 @@
 #include "arm_compute/core/ITensor.h"
 #include "arm_compute/core/Types.h"
 #include "arm_compute/core/Window.h"
+#include "src/core/helpers/WindowHelpers.h"
 
 namespace arm_compute
 {
@@ -14,16 +15,27 @@ void neon_vectorize_int_2_float32(const ITensor *src, const ITensor *vector, ITe
     std::cout << "window.y start " << window.y().start() << " end " << window.y().end() << " step " << window.y().step() << std::endl; 
     std::cout << "window.z start " << window.z().start() << " end " << window.z().end() << " step " << window.z().step() << std::endl; 
     
-    Window win = window;
-    win.set(Window::DimX, Window::Dimension(0,1,1));
-    win.set(Window::DimY, Window::Dimension(0,1,1));
-    const unsigned int window_start_x   = static_cast<unsigned int>(window.x().start());
-    const unsigned int window_end_x     = static_cast<unsigned int>(window.x().end());
+    /* Runtime reshape valid tensor region if input has been reshaped during preprocess */
+    size_t reshape_input_x = src->info()->valid_region().shape.x();
+    if(src->info()->tensor_shape().x() != reshape_input_x)
+    {
+        dst->info()->set_valid_region(dst->info()->valid_region().set(0,0,reshape_input_x));
+    }
+    Window win = calculate_max_window(dst->info()->valid_region());
+    std::cout << " reshaped " << std::endl;
+    std::cout << "window.x start " << win.x().start() << " end " << win.x().end() << " step " << win.x().step() << std::endl; 
+    std::cout << "window.y start " << win.y().start() << " end " << win.y().end() << " step " << win.y().step() << std::endl; 
+    std::cout << "window.z start " << win.z().start() << " end " << win.z().end() << " step " << win.z().step() << std::endl; 
+    
+    const unsigned int window_start_x   = static_cast<unsigned int>(win.x().start());
+    const unsigned int window_end_x     = static_cast<unsigned int>(win.x().end());
 
     const unsigned int vector_depth     = vector->info()->tensor_shape().y();
 
     unsigned int offset_vector,offset_dst;
 
+    win.set(Window::DimX, Window::Dimension(0,1,1));
+    win.set(Window::DimY, Window::Dimension(0,1,1));
     Iterator src_iter(src,win);
     Iterator dst_iter(dst,win);
     Iterator vector_iter(vector,win);
