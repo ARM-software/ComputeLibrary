cd9d127756796940cb1532253eed6ef2d3d3b5ad
dondavan <yeezushugh@gmail.com>
2024-04-21 23:07:23 +0200

    Verifying tensor

--- src/graph/GraphBuilder.cpp
+++ src/graph/GraphBuilder.cpp
@@ -1005,22 +1005,17 @@ NodeID GraphBuilder::add_embedding_node(Graph &g,
     g.add_connection(p_c_nid, 0, p_nid, 1);
 
     // Sum token embedding vector and segment embedding vector
-    NodeID add_nid_1 = g.add_node<EltwiseLayerNode>(descriptors::EltwiseLayerDescriptor{EltwiseOperation::Add});
-    // Sum previous vector and positional embedding vector
-    NodeID add_nid_2 = g.add_node<EltwiseLayerNode>(descriptors::EltwiseLayerDescriptor{EltwiseOperation::Add});
+    NodeID add_nid = g.add_node<EmbeddingSumLayerNode>(emb_info);
 
-    g.add_connection(t_nid, 0, add_nid_1, 0);
-    g.add_connection(s_nid, 0, add_nid_1, 1);
-
-    g.add_connection(add_nid_1, 0, add_nid_2, 0);
-    g.add_connection(p_nid, 0, add_nid_2, 1);
+    g.add_connection(t_nid, 0, add_nid, 0);
+    g.add_connection(s_nid, 0, add_nid, 1);
+    g.add_connection(p_nid, 0, add_nid, 2);
 
     set_node_params(g, t_nid, params);
     set_node_params(g, s_nid, params);
-    set_node_params(g, add_nid_1, params);
-    set_node_params(g, add_nid_2, params);
+    set_node_params(g, add_nid, params);
 
-    return add_nid_2;
+    return add_nid;
 }
 
 NodeID GraphBuilder::add_yolo_node(Graph &g, NodeParams params, NodeIdxPair input, ActivationLayerInfo act_info)
