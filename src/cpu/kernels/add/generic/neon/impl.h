796263db43e0a08510f8a54cd3bd59061aa8a009
dondavan <yeezushugh@gmail.com>
2024-04-22 16:05:55 +0200

    Verifying tensor

--- src/cpu/kernels/add/generic/neon/impl.h
+++ src/cpu/kernels/add/generic/neon/impl.h
@@ -109,8 +109,9 @@ void add_same_neon(
         // Clear X Dimension on execution window as we handle manually
         input1_win.set(Window::DimX, Window::Dimension(0, 1, 1));
         input2_win.set(Window::DimX, Window::Dimension(0, 1, 1));
-
+    
         // If valid region x has been runtime reshaped
+        /*
         if(src0->info()->valid_region().shape.x()!=src0->info()->tensor_shape().x())
         {
             Strides stride_src0 = compute_valid_strides(*src0->info());
@@ -133,6 +134,13 @@ void add_same_neon(
             Iterator output(dst, stride_dst, win);  
         }
         else Iterator output(dst, win);
+        */
+        Strides stride_src0 = compute_valid_strides(*src0->info());
+        Iterator input1(src0, stride_src0, input1_win); 
+        Strides stride_src1 = compute_valid_strides(*src1->info());
+        Iterator input2(src1, stride_src1, input2_win);
+        Strides stride_dst = compute_valid_strides(*dst->info());
+        Iterator output(dst, stride_dst, win);
         
         std::cout << "win.DimX()" << win.x().end() << std::endl;
         std::cout << "win.DimY()" << win.y().end() << std::endl;
