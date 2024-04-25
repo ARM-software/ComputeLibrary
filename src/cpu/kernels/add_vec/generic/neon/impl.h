1656a1e3d508fbc821ba1e9c3dd7fac50033c1ce
dondavan <yeezushugh@gmail.com>
2024-04-25 16:15:43 +0200

    Verifying tensor

--- src/cpu/kernels/add_vec/generic/neon/impl.h
+++ src/cpu/kernels/add_vec/generic/neon/impl.h
@@ -60,14 +60,14 @@ void add_vec_same_neon(
             const auto output_ptr = reinterpret_cast<ScalarType *>(output.ptr());
 
             std::cout << *reinterpret_cast<const ScalarType *>(src1->ptr_to_element(Coordinates(0,0))) << std::endl;
-            std::cout << *input1_ptr << std::endl;
+            std::cout << *input2_ptr << std::endl;
             // Compute S elements per iteration
             int x = window_start_target0;
             for (; x <= (window_end_target0 - window_step_target0); x += window_step_target0)
             {
                 for(int j =0; j <window_step_target0; j++)
                 {
-                    std::cout << *(reinterpret_cast<const ScalarType *>(input1_ptr + x)+j) << " ";
+                    std::cout << *(reinterpret_cast<const ScalarType *>(input2_ptr + x)+j) << " ";
                 }
                 const auto val1 = wrapper::vloadq(input1_ptr + x);
                 const auto val2 = wrapper::vloadq(input2_ptr + x);
