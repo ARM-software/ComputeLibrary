56313129dad6a0ffd0a05cd43b592fcf42498217
dondavan <yeezushugh@gmail.com>
2024-04-25 11:35:46 +0200

    Verifying tensor

--- src/cpu/kernels/CpuGemmMatrixMultiplyKernel.cpp
+++ src/cpu/kernels/CpuGemmMatrixMultiplyKernel.cpp
@@ -133,8 +133,8 @@ void CpuGemmMatrixMultiplyKernel::configure(const ITensorInfo     *lhs,
 
     // dst tensor auto inizialitation if not yet initialized
     TensorShape tensor_shape{lhs->tensor_shape()};
-    tensor_shape.set(0, is_interleaved ? reshape_info.n() : rhs->dimension(0));
-    tensor_shape.set(1, is_interleaved ? reshape_info.m() : lhs->dimension(1));
+    tensor_shape.set(0, is_interleaved ? reshape_info.m() : rhs->dimension(0));
+    tensor_shape.set(1, is_interleaved ? reshape_info.n() : lhs->dimension(1));
     auto_init_if_empty(*dst, lhs->clone()->set_tensor_shape(tensor_shape));
 
     // Perform validate step
