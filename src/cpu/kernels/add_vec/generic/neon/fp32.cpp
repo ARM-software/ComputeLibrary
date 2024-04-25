849757120d590275399defd6d3d922c65913798c
dondavan <yeezushugh@gmail.com>
2024-04-25 12:38:28 +0200

    Verifying tensor

--- src/cpu/kernels/add_vec/generic/neon/fp32.cpp
+++ src/cpu/kernels/add_vec/generic/neon/fp32.cpp
@@ -4,7 +4,7 @@ namespace arm_compute
 {
 namespace cpu
 {
-void add_fp32_neon(
+void add_vec_fp32_neon(
     const ITensor *src0, const ITensor *src1, ITensor *dst, const ConvertPolicy &policy, const Window &window)
 {
     return add_same_neon<float>(src0, src1, dst, policy, window);
