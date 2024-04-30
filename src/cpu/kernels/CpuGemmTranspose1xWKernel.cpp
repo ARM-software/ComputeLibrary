7498016ea2b7d0919c92812118132514f98ee3ec
dondavan <yeezushugh@gmail.com>
2024-04-30 13:16:14 +0200

    Verifying tensor

--- src/cpu/kernels/CpuGemmTranspose1xWKernel.cpp
+++ src/cpu/kernels/CpuGemmTranspose1xWKernel.cpp
@@ -152,8 +152,8 @@ void CpuGemmTranspose1xWKernel::run_op(ITensorPack &tensors, const Window &windo
                 else
                 {
                     std::memcpy(out_ptr + k * element_size, in_ptr + k * element_size, element_size);
-                    count++;
                     if(count%768 ==0)std::cout << *reinterpret_cast<const float *>(in_ptr + k * element_size) << std::endl;
+                    count++;
                 }
             }
         },
