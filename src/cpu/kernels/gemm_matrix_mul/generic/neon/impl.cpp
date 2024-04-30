ffaa6a254bc38e54c0eab83a6726e5ee255fe6ac
dondavan <yeezushugh@gmail.com>
2024-04-30 18:05:14 +0200

    Verifying tensor

--- src/cpu/kernels/gemm_matrix_mul/generic/neon/impl.cpp
+++ src/cpu/kernels/gemm_matrix_mul/generic/neon/impl.cpp
@@ -306,6 +306,13 @@ void matrix_matrix_multiply_f32(
     win_b.set(Window::DimX, Window::Dimension(window.x().start() / 4, window.x().end() / 4, 2 * in_b_stride));
     win_b.set(Window::DimY, Window::Dimension(0, 0, 0));
 
+    std::cout << "b_to_use (0,0) " << *reinterpret_cast<float *> (rhs->ptr_to_element(Coordinates(0,0))) << std::endl;
+    std::cout << "b_to_use (1,0) " << *reinterpret_cast<float *> (rhs->ptr_to_element(Coordinates(1,0))) << std::endl;
+    std::cout << "b_to_use (2,0) " << *reinterpret_cast<float *> (rhs->ptr_to_element(Coordinates(2,0))) << std::endl;
+    std::cout << "b_to_use (3,0) " << *reinterpret_cast<float *> (rhs->ptr_to_element(Coordinates(3,0))) << std::endl;
+    std::cout << "b_to_use (4,0) " << *reinterpret_cast<float *> (rhs->ptr_to_element(Coordinates(4,0))) << std::endl;
+    std::cout << "b_to_use (0,1) " << *reinterpret_cast<float *> (rhs->ptr_to_element(Coordinates(0,1))) << std::endl;
+
     Iterator ina(lhs, win_a);
     Iterator inb(rhs, win_b);
     Iterator out(dst, window);
@@ -325,9 +332,9 @@ void matrix_matrix_multiply_f32(
             auto mtx_b0 = reinterpret_cast<const float *>(inb.ptr());
             auto mtx_b1 = mtx_b0 + in_b_stride;
 
-            std::cout << "mtx_a0 " << *mtx_a0 << std::endl;
-            std::cout << "mtx_b0 " << *mtx_b0 << std::endl;
-            std::cout << "mtx_b1 " << *mtx_b1 << std::endl;
+            //std::cout << "mtx_a0 " << *mtx_a0 << std::endl;
+            //std::cout << "mtx_b0 " << *mtx_b0 << std::endl;
+            //std::cout << "mtx_b1 " << *mtx_b1 << std::endl;
 
             float32x4_t acc00 = vdupq_n_f32(0.f);
             float32x4_t acc10 = vdupq_n_f32(0.f);
