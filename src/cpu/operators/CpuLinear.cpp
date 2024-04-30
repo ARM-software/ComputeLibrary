9bcd9aa901e2afcbe2cf812e71acb35b8ea536ab
dondavan <yeezushugh@gmail.com>
2024-04-30 18:50:11 +0200

    Verifying tensor

--- src/cpu/operators/CpuLinear.cpp
+++ src/cpu/operators/CpuLinear.cpp
@@ -123,10 +123,27 @@ void CpuLinear::run(ITensorPack &tensors)
 
     ITensorPack mm_pack{{ACL_SRC_0, a}, {ACL_SRC_1, b}, {ACL_DST, (_run_bias_addition) ? temp_d.get() : d}};
 
-    
+    if (_run_interleave_transpose)
+    {
+        // Run interleave kernel
+        ITensorPack interleave_pack{{ACL_SRC, a}, {ACL_DST, interleaved_a.get()}};
+        NEScheduler::get().schedule_op(_interleave_kernel.get(), Window::DimY, _interleave_kernel->window(),
+                                        interleave_pack);
+        // Use reshaped matrices
+        mm_pack.add_const_tensor(ACL_SRC_0, interleaved_a.get());
+    }
+
     const ITensor *b_to_use = b;
 
-    
+    if (_run_interleave_transpose)
+    {
+        // Run transpose1xw kernel
+        ITensorPack transpose_pack{{ACL_SRC, b_to_use}, {ACL_DST, transposed1xw_b.get()}};
+        NEScheduler::get().schedule_op(_transpose1xW_b_kernel.get(), Window::DimY,
+                                        _transpose1xW_b_kernel->window(), transpose_pack);
+
+        b_to_use = transposed1xw_b.get();
+    }
 
     // Use reshaped matrices
     mm_pack.add_const_tensor(ACL_SRC_1, b_to_use);
