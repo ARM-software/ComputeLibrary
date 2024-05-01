7d0f970c5a14fe85ac232a2820f92c49229df1dd
dondavan <yeezushugh@gmail.com>
2024-05-01 16:55:30 +0200

    reshape

--- src/cpu/kernels/CpuVectorizeKernel.cpp
+++ src/cpu/kernels/CpuVectorizeKernel.cpp
@@ -67,7 +67,7 @@ void CpuVectorizeKernel::configure(const ITensorInfo *src, const ITensorInfo *ve
     ICPPKernel::configure(win);
     std::cout << "win.x start " << win.x().start() << " end " << win.x().end() << " step " << win.x().step() << std::endl; 
     std::cout << "win.y start " << win.y().start() << " end " << win.y().end() << " step " << win.y().step() << std::endl; 
-    std::cout << "win.z start " << win.z().start() << " end " << win.z().end() << " step " << winz().step() << std::endl; 
+    std::cout << "win.z start " << win.z().start() << " end " << win.z().end() << " step " << win.z().step() << std::endl; 
     
     
 }
