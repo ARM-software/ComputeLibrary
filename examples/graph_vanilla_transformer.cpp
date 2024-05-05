8b2638d7acbb49d58957d495b832a6d3bc8b49a1
dondavan <yeezushugh@gmail.com>
2024-05-05 10:39:56 +0200

    reshape

--- examples/graph_vanilla_transformer.cpp
+++ examples/graph_vanilla_transformer.cpp
@@ -69,7 +69,7 @@ public:
         constexpr unsigned int d_segemnt     = 2U;       // Sentence segmentation size
         constexpr unsigned int d_position    = 512U;     // Pretrained positional encoding length
         constexpr unsigned int h             = 12U;      // Parallel attention (Heads)
-        constexpr float        eps           = 1e-5;     // Layer normalization eplision
+        constexpr float        eps           = 1e-12;     // Layer normalization eplision
         constexpr unsigned int d_ff          = 3072U;    // Dim feedforward
         /*constexpr unsigned int d_q         = 64U;      // Dim query, 512U/8U
         constexpr unsigned int d_k           = 64U;      // Dim key, 512U/8U
