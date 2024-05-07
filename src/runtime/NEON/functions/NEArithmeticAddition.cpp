55b9f93de0e4e92b9ff66bd76afcfbd96687e8a2
dondavan <yeezushugh@gmail.com>
2024-05-07 10:55:44 +0200

    reshape

--- src/runtime/NEON/functions/NEArithmeticAddition.cpp
+++ src/runtime/NEON/functions/NEArithmeticAddition.cpp
@@ -75,6 +75,30 @@ void NEArithmeticAddition::run()
     pack.add_tensor(TensorType::ACL_SRC_0, _impl->src_0);
     pack.add_tensor(TensorType::ACL_SRC_1, _impl->src_1);
     pack.add_tensor(TensorType::ACL_DST, _impl->dst);
+
+    std::cout <<"src/runtime/NEON/functions/NELayerNormLayer.cpp src 0 x: " << _impl->src_0->info()->tensor_shape().x() << std::endl;
+    std::cout <<"src/runtime/NEON/functions/NELayerNormLayer.cpp src 0 y: " << _impl->src_0->info()->tensor_shape().y() << std::endl;
+    std::cout <<"src/runtime/NEON/functions/NELayerNormLayer.cpp src 0 z: " << _impl->src_0->info()->tensor_shape().z() << std::endl;
+    std::cout << *reinterpret_cast<float *>(_impl->src_0->ptr_to_element(Coordinates(0,0)))  << std::endl;
+    std::cout << *reinterpret_cast<float *>(_impl->src_0->ptr_to_element(Coordinates(0,1)))  << std::endl;
+
+    std::cout << *reinterpret_cast<float *>(_impl->src_0->ptr_to_element(Coordinates(1,0,0)))  << std::endl;
+    std::cout << *reinterpret_cast<float *>(_impl->src_0->ptr_to_element(Coordinates(2,0,0)))  << std::endl;
+    std::cout << *reinterpret_cast<float *>(_impl->src_0->ptr_to_element(Coordinates(767,0,0)))  << std::endl;
+    std::cout << *reinterpret_cast<float *>(_impl->src_0->ptr_to_element(Coordinates(768,0,0)))  << std::endl;
+
+
+    std::cout <<"src/runtime/NEON/functions/NELayerNormLayer.cpp src 1 x: " << _impl->src_1->info()->tensor_shape().x() << std::endl;
+    std::cout <<"src/runtime/NEON/functions/NELayerNormLayer.cpp src 1 y: " << _impl->src_1->info()->tensor_shape().y() << std::endl;
+    std::cout <<"src/runtime/NEON/functions/NELayerNormLayer.cpp src 1 z: " << _impl->src_1->info()->tensor_shape().z() << std::endl;
+    std::cout << *reinterpret_cast<float *>(_impl->src_1->ptr_to_element(Coordinates(0,0)))  << std::endl;
+    std::cout << *reinterpret_cast<float *>(_impl->src_1->ptr_to_element(Coordinates(0,1)))  << std::endl;
+
+    std::cout << *reinterpret_cast<float *>(_impl->src_1->ptr_to_element(Coordinates(1,0,0)))  << std::endl;
+    std::cout << *reinterpret_cast<float *>(_impl->src_1->ptr_to_element(Coordinates(2,0,0)))  << std::endl;
+    std::cout << *reinterpret_cast<float *>(_impl->src_1->ptr_to_element(Coordinates(767,0,0)))  << std::endl;
+    std::cout << *reinterpret_cast<float *>(_impl->src_1->ptr_to_element(Coordinates(768,0,0)))  << std::endl;
+
     _impl->op->run(pack);
 }
 } // namespace arm_compute
