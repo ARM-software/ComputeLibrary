c9f7d47aa45d041a9d5f5c14fe2a7510d3760ece
Pablo Marquez Tello <pablo.tello@arm.com>
2024-07-11 10:57:07 +0100

    Revert "Add dedicated FP32 SME2 Logistic Activation Kernel"
    This reverts commit 8dc9b944a471f50403efa46924aa8b9de35cf838.
    
    Signed-off-by: Pablo Marquez Tello <pablo.tello@arm.com>
    Change-Id: I2340c65620102e3dc2075c34470cdbccfc9b690a
    Reviewed-on: https://review.mlplatform.org/c/ml/ComputeLibrary/+/11865
    Reviewed-by: Ramy Elgammal <ramy.elgammal@arm.com>
    Comments-Addressed: Arm Jenkins <bsgcomp@arm.com>
    Benchmark: Arm Jenkins <bsgcomp@arm.com>
    Tested-by: Arm Jenkins <bsgcomp@arm.com>

--- src/cpu/kernels/CpuActivationKernel.cpp
+++ src/cpu/kernels/CpuActivationKernel.cpp
@@ -32,7 +32,6 @@
 #include "src/core/helpers/AutoConfiguration.h"
 #include "src/core/helpers/WindowHelpers.h"
 #include "src/cpu/kernels/activation/list.h"
-#include "src/cpu/kernels/logistic/list.h"
 
 #include <array>
 
@@ -72,12 +71,6 @@ static const std::vector<CpuActivationKernel::ActivationKernel> available_kernel
      },
      REGISTER_Q8_NEON(arm_compute::cpu::neon_q8_activation_lut)},
 #endif // __aarch64__
-    {"sme2_fp32_logistic",
-     [](const ActivationDataTypeISASelectorData &data) {
-         return data.dt == DataType::F32 && data.f == ActivationLayerInfo::ActivationFunction::LOGISTIC &&
-                data.isa.sme2;
-     },
-     REGISTER_FP32_SME2(arm_compute::cpu::sme2_fp32_logistic)},
     {"sve2_qu8_activation",
      [](const ActivationDataTypeISASelectorData &data) {
          return data.dt == DataType::QASYMM8 && data.isa.sve2 &&
@@ -323,11 +316,6 @@ void CpuActivationKernel::configure(const ITensorInfo *src, ITensorInfo *dst, Ac
 
     // Use squashed window
     std::tie(win, _split_dimension) = calculate_squashed_or_max_window(*src);
-    // Collapse window with SME kernels in Y-Dim to handle padding
-    if (std::string(uk->name) == "sme2_fp32_logistic")
-    {
-        win = win.collapse(win, Window::DimY);
-    }
     ICPPKernel::configure(win);
 }
 
