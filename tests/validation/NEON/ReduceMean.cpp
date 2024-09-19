4216a7395492382ba07f9383a19f83425653e26f
Gunes Bayir <gunes.bayir@arm.com>
2024-09-19 22:36:30 +0100

    fix: Apply consistent rounding policy in NEReduceMean
    The rounding behavior in this operator was not consistent, sometimes truncating, sometimes rounding to the closest integer. The truncation usually should not be used because it'll cause a value like 1.999 to become 1, which is not a good approximation. Also, the rounding policy while converting from float to int has been changed to Round Closest to Integer with Ties to Nearest Even; instead of Round to Closest with Ties Away From Zero because it'll have less bias.
    
    An Example: (139 + 154 + 164 + 179) / 4 is 159 w/o the need of rounding. However, if we have to do this operation recursively/repetitively, i.e.
    
    ```
    mean( mean(139, 154), mean(164, 179) )
    ```
    The results are going to be different depending on the rounding policy chosen.
    
    With Ties Away From Zero:
    ```
    mean(139, 154) = (139 + 154) / 2 = 146.5 --> 147
    mean(164, 179) = (164 + 179) / 2 = 171.5 --> 172
    mean(147, 172) = (147 + 172) / 2 = 159.5 --> 160
    ```
    where we round up (away from zero) in both cases, causing an addition of 0.5 in both scenarios. And the final result changes by 1.
    
    With Ties to Nearest Even:
    ```
    mean(139, 154) = (139 + 154) / 2 = 146.5 --> 146
    mean(164, 179) = (164 + 179) / 2 = 171.5 --> 172
    mean(147, 172) = (146 + 172) / 2 = 159 --> 159
    ```
    where we round down in the former and up in the latter, which balances the total, and we have the same result with the 4-element mean.
    
    This occurs because when total, T, is divided into two pieces, X and T-X, the it's either Odd + Odd or Even + Even (if T is even as in our case). In the case where we require rounding, i.e. Odd+Odd, if X is rounded down, T-X will be rounded up because (T-X)/2 = T/2 (Integer) - X/2. And, X/2 and -X/2 will be rounded towards the same absolute value, i.e. if 61 / 2 --> 30 then -61/2 --> -30. In one, the closest even number is towards zero (i.e. 30.5 --> 30), so it's rounded down, and in the other one, it's rounded up (i.e. -30.5 --> -30). This balances to total sum.
    
    This rounding mode has also been chosen by other ML frameworks for similar reasons.
    
    Resolves: COMPMID-7496
    
    Signed-off-by: Gunes Bayir <gunes.bayir@arm.com>
    Change-Id: I9173078e1678a6231b978a1f84f6c01ed3989428
    Reviewed-on: https://review.mlplatform.org/c/ml/ComputeLibrary/+/12489
    Tested-by: Arm Jenkins <bsgcomp@arm.com>
    Reviewed-by: Sang Won Ha <sangwon.ha@arm.com>
    Comments-Addressed: Arm Jenkins <bsgcomp@arm.com>
    Benchmark: Arm Jenkins <bsgcomp@arm.com>

--- tests/validation/NEON/ReduceMean.cpp
+++ tests/validation/NEON/ReduceMean.cpp
@@ -34,6 +34,10 @@
 #include "tests/validation/Validation.h"
 #include "tests/validation/fixtures/ReduceMeanFixture.h"
 
+#include <algorithm>
+#include <cstdint>
+#include <vector>
+
 namespace arm_compute
 {
 namespace test
@@ -54,12 +58,96 @@ constexpr AbsoluteTolerance<uint8_t> tolerance_u8(2);    /**< Tolerance value fo
 constexpr AbsoluteTolerance<int8_t>  tolerance_s8(2);    /**< Tolerance value for comparing reference's output against implementation's output for signed 8-bit asymmetric quantized type */
 #endif // __aarch64__
 
+constexpr AbsoluteTolerance<uint8_t> zero_tolerance_u8(0);
+constexpr AbsoluteTolerance<int8_t>  zero_tolerance_s8(0);
+
 const auto axis_keep = combine(framework::dataset::make("Axis", { Coordinates(0), Coordinates(1, 0), Coordinates(1, 2), Coordinates(0, 2), Coordinates(1, 3), Coordinates(2, 3), Coordinates(0, 1, 2, 3) }),
                                framework::dataset::make("KeepDims", { true }));
 const auto axis_drop = combine(framework::dataset::make("Axis", { Coordinates(0), Coordinates(1), Coordinates(3) }), framework::dataset::make("KeepDims", { false }));
 } // namespace
 TEST_SUITE(NEON)
 TEST_SUITE(ReduceMean)
+
+TEST_CASE(ProperRoundingPolicyXReduction, framework::DatasetMode::ALL)
+{
+    // We do not need to stress vector and leftover loops diffrently
+    // because the rounding is done scalarly at the end. Accumulation
+    // is done over integer types.
+    constexpr int x_len = 2;
+
+    const auto input_shape = TensorShape(x_len);
+    const auto output_shape = TensorShape(1);
+    const bool keep_dims = true;
+    const auto axis = Coordinates(0);
+    const auto input_qinfo = QuantizationInfo(2 / 255.f, 0);
+    const auto output_qinfo = QuantizationInfo(6 / 255.f, -1);
+    const auto dtype = DataType::QASYMM8_SIGNED;
+
+    Tensor input = create_tensor<Tensor>(input_shape, dtype, 1, input_qinfo);
+    Tensor output = create_tensor<Tensor>(output_shape, dtype, 1, output_qinfo);
+
+    NEReduceMean reduce_mean;
+    reduce_mean.configure(&input, axis, keep_dims, &output);
+
+    input.allocator()->allocate();
+    output.allocator()->allocate();
+
+    std::vector<int8_t> values {50, 26};
+    library->fill_static_values(Accessor(input), values);
+
+    std::vector<int8_t> expected {12};
+    SimpleTensor<int8_t> ref{ output_shape, dtype, 1, input_qinfo };
+    library->fill_static_values(ref, expected);
+
+    reduce_mean.run();
+
+    // The tolerance should be 0 because this test stresses the rounding behavior of the operator
+    validate(Accessor(output), ref, zero_tolerance_s8);
+}
+
+#ifdef __aarch64__
+// Due to the lack of instructions in a32, the rounding operation is less
+// accurate
+TEST_CASE(ProperRoundingPolicyNonXReduction, framework::DatasetMode::ALL)
+{
+    constexpr int x_len = 17; // > 16 to stress both vector and leftover loops
+
+    const auto input_shape = TensorShape(x_len, 2, 2, 1);
+    const auto output_shape = TensorShape(x_len, 1, 1, 1);
+    const bool keep_dims = true;
+    const auto axis = Coordinates(1, 2);
+    const auto input_qinfo = QuantizationInfo(2 / 255.f, 127);
+    const auto output_qinfo = QuantizationInfo(2 / 255.f, 127);
+    const auto dtype = DataType::QASYMM8;
+
+    Tensor input = create_tensor<Tensor>(input_shape, dtype, 1, input_qinfo);
+    Tensor output = create_tensor<Tensor>(output_shape, dtype, 1, output_qinfo);
+
+    NEReduceMean reduce_mean;
+    reduce_mean.configure(&input, axis, keep_dims, &output);
+
+    input.allocator()->allocate();
+    output.allocator()->allocate();
+
+    // {139, 139 ... 139 (x_len times) 154, 154, ... 154 (x_len_times) ...}
+    std::vector<uint8_t> values;
+    fill_n(back_inserter(values), x_len, 139);
+    fill_n(back_inserter(values), x_len, 154);
+    fill_n(back_inserter(values), x_len, 164);
+    fill_n(back_inserter(values), x_len, 179);
+    library->fill_static_values(Accessor(input), values);
+
+    std::vector<uint8_t> expected;
+    fill_n(back_inserter(expected), x_len, 159); // 159 = (139 + 154 + 164 + 179) / 4
+    SimpleTensor<uint8_t> ref{ output_shape, dtype, 1, input_qinfo };
+    library->fill_static_values(ref, expected);
+
+    reduce_mean.run();
+
+    // The tolerance should be 0 because this test stresses the rounding behavior of the operator
+    validate(Accessor(output), ref, zero_tolerance_u8);
+}
+#endif // __aarch64__
 
 // *INDENT-OFF*
 // clang-format off
