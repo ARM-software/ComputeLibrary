d75ed66c085901ccd0a0cee96986606ccc6bebd2
Omar Al Khatib <omar.alkhatib@arm.com>
2025-01-10 10:25:59 +0000

    test: Add thread safety tests for CpuMul
    Resolves: COMPMID-7861
    
    Signed-off-by: Omar Al Khatib <omar.alkhatib@arm.com>
    Change-Id: I7c4bf56e9711f7c6fec70bafb7e15d6e2bdcda02
    Reviewed-on: https://review.mlplatform.org/c/ml/ComputeLibrary/+/13444
    Tested-by: Arm Jenkins <bsgcomp@arm.com>
    Reviewed-by: Gunes Bayir <gunes.bayir@arm.com>
    Comments-Addressed: Arm Jenkins <bsgcomp@arm.com>
    Benchmark: Arm Jenkins <bsgcomp@arm.com>

--- tests/validation/fixtures/CpuMulFixture.h
+++ tests/validation/fixtures/CpuMulFixture.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2017-2021, 2023-2024 Arm Limited.
+ * Copyright (c) 2017-2021, 2023-2025 Arm Limited.
  *
  * SPDX-License-Identifier: MIT
  *
@@ -31,9 +31,13 @@
 #include "tests/framework/Asserts.h"
 #include "tests/framework/Fixture.h"
 #include "tests/Globals.h"
-#include "tests/IAccessor.h"
 #include "tests/validation/reference/ActivationLayer.h"
 #include "tests/validation/reference/PixelWiseMultiplication.h"
+
+#if !defined(BARE_METAL)
+#include <thread>
+#include <vector>
+#endif // !defined(BARE_METAL)
 
 namespace arm_compute
 {
@@ -41,7 +45,11 @@ namespace test
 {
 namespace validation
 {
-template <typename TensorType, typename AccessorType, typename FunctionType, typename T1, typename T2, typename T3 = T2>
+namespace
+{
+constexpr int NUM_THREADS =  3;
+}// namespace
+template <typename TensorType, typename AccessorType, typename FunctionType, typename T1, typename T2, typename T3>
 class CpuMulGenericValidationFixture : public framework::Fixture
 {
 public:
@@ -53,7 +61,11 @@ public:
                float              scale,
                ConvertPolicy      convert_policy,
                RoundingPolicy     rounding_policy,
-               bool               is_inplace)
+               bool               is_inplace,
+               QuantizationInfo    qinfo1,
+               QuantizationInfo    qinfo2,
+               QuantizationInfo    qinfo_out,
+               TestType            test_type)
     {
         if (std::is_same<TensorType, Tensor>::value && // Cpu
             (dt_in1 == DataType::F16 || dt_in2 == DataType::F16 || dt_out == DataType::F16) &&
@@ -63,11 +75,15 @@ public:
         }
 
         _is_inplace = is_inplace;
-        _target     = compute_target(shape0, shape1, dt_in1, dt_in2, dt_out, scale, convert_policy, rounding_policy,
-                                     QuantizationInfo(), QuantizationInfo(), QuantizationInfo(), ActivationLayerInfo());
-        _reference =
-            compute_reference(shape0, shape1, dt_in1, dt_in2, dt_out, scale, convert_policy, rounding_policy,
-                              QuantizationInfo(), QuantizationInfo(), QuantizationInfo(), ActivationLayerInfo());
+        _test_type = test_type;
+
+        _num_parallel_runs        = (_test_type == TestType::ConfigureOnceRunMultiThreaded ? NUM_THREADS : 1);
+
+        compute_target(shape0, shape1, dt_in1, dt_in2, dt_out, scale, convert_policy, rounding_policy,
+                                     qinfo1, qinfo2, qinfo_out, ActivationLayerInfo());
+
+        compute_reference(shape0, shape1, dt_in1, dt_in2, dt_out, scale, convert_policy, rounding_policy,
+                              qinfo1, qinfo2, qinfo_out, ActivationLayerInfo());
     }
 
 protected:
@@ -77,7 +93,31 @@ protected:
         library->fill_tensor_uniform(tensor, seed_offset);
     }
 
-    TensorType compute_target(const TensorShape  &shape0,
+    void allocate_and_fill_tensors(TensorType *src1, TensorType *src2, TensorType *dst){
+        auto allocate_tensor = [](TensorType &t)
+        {
+            ARM_COMPUTE_ASSERT(t.info()->is_resizable());
+            t.allocator()->allocate();
+            ARM_COMPUTE_ASSERT(!t.info()->is_resizable());
+        };
+
+        for(int i = 0; i < _num_parallel_runs; ++i){
+            allocate_tensor(src1[i]);
+            allocate_tensor(src2[i]);
+
+            // If don't do in-place computation, still need to allocate original dst
+            if (!_is_inplace)
+            {
+                allocate_tensor(dst[i]);
+            }
+
+            // Fill tensors
+            fill(AccessorType(src1[i]), (2*i + 0));
+            fill(AccessorType(src2[i]), (2*i + 1));
+        }
+    }
+
+    void compute_target(const TensorShape  &shape0,
                               const TensorShape  &shape1,
                               DataType            dt_in1,
                               DataType            dt_in2,
@@ -91,13 +131,25 @@ protected:
                               ActivationLayerInfo act_info)
     {
         // Create tensors
+        TensorType src1[NUM_THREADS];
+        TensorType src2[NUM_THREADS];
+        TensorType dst[NUM_THREADS];
+
+        TensorType *dst_ptrs[NUM_THREADS];
+
+        ITensorPack run_pack[NUM_THREADS];
+
         const TensorShape out_shape = TensorShape::broadcast_shape(shape0, shape1);
-        TensorType        src1      = create_tensor<TensorType>(shape0, dt_in1, 1, qinfo0);
-        TensorType        src2      = create_tensor<TensorType>(shape1, dt_in2, 1, qinfo1);
-        TensorType        dst       = create_tensor<TensorType>(out_shape, dt_out, 1, qinfo_out);
+
+        for(int i = 0; i < _num_parallel_runs; ++i){
+            src1[i] = create_tensor<TensorType>(shape0, dt_in1, 1, qinfo0);
+            src2[i] = create_tensor<TensorType>(shape1, dt_in2, 1, qinfo1);
+            dst[i]  = create_tensor<TensorType>(out_shape, dt_out, 1, qinfo_out);
+            dst_ptrs[i] = &dst[i];
+
+        }
 
         // Check whether do in-place computation and whether inputs are broadcast compatible
-        TensorType *actual_dst = &dst;
         if (_is_inplace)
         {
             bool src1_is_inplace = !arm_compute::detail::have_different_dimensions(out_shape, shape0, 0) &&
@@ -107,51 +159,57 @@ protected:
             bool do_in_place = out_shape.total_size() != 0 && (src1_is_inplace || src2_is_inplace);
             ARM_COMPUTE_ASSERT(do_in_place);
 
-            if (src1_is_inplace)
-            {
-                actual_dst = &src1;
-            }
-            else
-            {
-                actual_dst = &src2;
+            for(int i = 0; i < _num_parallel_runs; ++i){
+                dst_ptrs[i] = src1_is_inplace ? &(src1[i]) : &(src2[i]);
             }
         }
-
-        auto allocate_tensor = [](TensorType &t)
-        {
-            ARM_COMPUTE_ASSERT(t.info()->is_resizable());
-            t.allocator()->allocate();
-            ARM_COMPUTE_ASSERT(!t.info()->is_resizable());
-        };
 
         // Create and configure function
         FunctionType multiply;
-        multiply.configure(src1.info(), src2.info(), actual_dst->info(), scale, convert_policy, rounding_policy,
+        multiply.configure(src1[0].info(), src2[0].info(), dst_ptrs[0]->info(), scale, convert_policy, rounding_policy,
                            act_info);
 
-        allocate_tensor(src1);
-        allocate_tensor(src2);
+        allocate_and_fill_tensors(src1, src2, dst);
 
-        // If don't do in-place computation, still need to allocate original dst
-        if (!_is_inplace)
+         if(_test_type == TestType::ConfigureOnceRunMultiThreaded)
         {
-            allocate_tensor(dst);
+#ifndef BARE_METAL
+            std::vector<std::thread> threads;
+
+            threads.reserve(_num_parallel_runs);
+            for(int i = 0; i < _num_parallel_runs; ++i)
+            {
+                // Compute function
+                run_pack[i] = { { arm_compute::TensorType::ACL_SRC_0, &src1[i] },
+                {arm_compute::TensorType::ACL_SRC_1, &src2[i]},
+                {arm_compute::TensorType::ACL_DST, dst_ptrs[i]}};
+
+                threads.emplace_back([&,i]
+                {
+
+                    multiply.run(run_pack[i]);
+                    _target[i] =std::move(*(dst_ptrs[i]));
+                });
+            }
+            for(int i = 0; i < _num_parallel_runs; ++i)
+            {
+                threads[i].join();
+            }
+#endif // ifndef BARE_METAL
         }
+        else
+        {
+            // Compute function
+            ITensorPack run_pack{{arm_compute::TensorType::ACL_SRC_0, &src1[0]},
+                                {arm_compute::TensorType::ACL_SRC_1, &src2[0]},
+                                {arm_compute::TensorType::ACL_DST, dst_ptrs[0]}};
+            multiply.run(run_pack);
 
-        // Fill tensors
-        fill(AccessorType(src1), 0);
-        fill(AccessorType(src2), 1);
-
-        // Compute function
-        ITensorPack run_pack{{arm_compute::TensorType::ACL_SRC_0, &src1},
-                             {arm_compute::TensorType::ACL_SRC_1, &src2},
-                             {arm_compute::TensorType::ACL_DST, actual_dst}};
-        multiply.run(run_pack);
-
-        return std::move(*actual_dst);
+            _target[0] = std::move(*(dst_ptrs[0]));
+        }
     }
 
-    SimpleTensor<T3> compute_reference(const TensorShape  &shape0,
+    void compute_reference(const TensorShape  &shape0,
                                        const TensorShape  &shape1,
                                        DataType            dt_in1,
                                        DataType            dt_in2,
@@ -168,21 +226,25 @@ protected:
         SimpleTensor<T1> src1{shape0, dt_in1, 1, qinfo0};
         SimpleTensor<T2> src2{shape1, dt_in2, 1, qinfo1};
 
-        // Fill reference
-        fill(src1, 0);
-        fill(src2, 1);
-
-        auto result = reference::pixel_wise_multiplication<T1, T2, T3>(src1, src2, scale, convert_policy,
-                                                                       rounding_policy, dt_out, qinfo_out);
-        return act_info.enabled() ? reference::activation_layer(result, act_info, qinfo_out) : result;
+        for(int i = 0; i < _num_parallel_runs; ++i)
+        {
+            // Fill reference
+            fill(src1, 2*i + 0);
+            fill(src2, 2*i + 1);
+            auto result = reference::pixel_wise_multiplication<T1, T2, T3>(src1, src2, scale, convert_policy,
+                                                                        rounding_policy, dt_out, qinfo_out);
+            _reference[i] = act_info.enabled() ? reference::activation_layer(result, act_info, qinfo_out) : result;
+        }
     }
 
-    TensorType       _target{};
-    SimpleTensor<T3> _reference{};
-    bool             _is_inplace{false};
+    TensorType                     _target[NUM_THREADS];
+    SimpleTensor<T3>               _reference[NUM_THREADS];
+    bool                           _is_inplace{false};
+    TestType                       _test_type{};
+    int                            _num_parallel_runs{};
 };
 
-template <typename TensorType, typename AccessorType, typename FunctionType, typename T1, typename T2, typename T3 = T2>
+template <typename TensorType, typename AccessorType, typename FunctionType, typename T1, typename T2, typename T3>
 class CpuMulValidationFixture
     : public CpuMulGenericValidationFixture<TensorType, AccessorType, FunctionType, T1, T2, T3>
 {
@@ -197,7 +259,48 @@ public:
                bool               is_inplace)
     {
         CpuMulGenericValidationFixture<TensorType, AccessorType, FunctionType, T1, T2, T3>::setup(
-            shape, shape, dt_in1, dt_in2, dt_out, scale, convert_policy, rounding_policy, is_inplace);
+            shape, shape, dt_in1, dt_in2, dt_out, scale, convert_policy, rounding_policy, is_inplace, QuantizationInfo(), QuantizationInfo(), QuantizationInfo(), TestType::ConfigureOnceRunOnce);
+    }
+};
+
+template <typename TensorType, typename AccessorType, typename FunctionType, typename T1, typename T2, typename T3>
+class CpuMulThreadSafeValidationFixture
+    : public CpuMulGenericValidationFixture<TensorType, AccessorType, FunctionType, T1, T2, T3>
+{
+public:
+    void setup(const TensorShape &shape,
+               DataType           dt_in1,
+               DataType           dt_in2,
+               DataType           dt_out,
+               float              scale,
+               ConvertPolicy      convert_policy,
+               RoundingPolicy     rounding_policy,
+               bool               is_inplace)
+    {
+        CpuMulGenericValidationFixture<TensorType, AccessorType, FunctionType, T1, T2, T3>::setup(
+            shape, shape, dt_in1, dt_in2, dt_out, scale, convert_policy, rounding_policy, is_inplace, QuantizationInfo(), QuantizationInfo(), QuantizationInfo(), TestType::ConfigureOnceRunMultiThreaded);
+    }
+};
+
+template <typename TensorType, typename AccessorType, typename FunctionType, typename T1, typename T2, typename T3>
+class CpuMulQuantizedThreadSafeValidationFixture
+    : public CpuMulGenericValidationFixture<TensorType, AccessorType, FunctionType, T1, T2, T3>
+{
+public:
+    void setup(const TensorShape &shape,
+               DataType           dt_in1,
+               DataType           dt_in2,
+               DataType           dt_out,
+               float              scale,
+               ConvertPolicy      convert_policy,
+               RoundingPolicy     rounding_policy,
+               QuantizationInfo qinfo1,
+               QuantizationInfo qinfo2,
+               QuantizationInfo    qinfo_out,
+               bool               is_inplace)
+    {
+        CpuMulGenericValidationFixture<TensorType, AccessorType, FunctionType, T1, T2, T3>::setup(
+            shape, shape, dt_in1, dt_in2, dt_out, scale, convert_policy, rounding_policy, is_inplace, qinfo1, qinfo2, qinfo_out, TestType::ConfigureOnceRunMultiThreaded);
     }
 };
 
--- tests/validation/runtime/experimental/operators/CpuMul.cpp
+++ tests/validation/runtime/experimental/operators/CpuMul.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2017-2021, 2024 Arm Limited.
+ * Copyright (c) 2017-2021, 2024-2025 Arm Limited.
  *
  * SPDX-License-Identifier: MIT
  *
@@ -39,10 +39,16 @@ namespace test
 {
 namespace validation
 {
+using framework::dataset::make;
 namespace
 {
 const float scale_255   = 1.f / 255.f;
 const float scale_other = 1.f / 32768.f;
+const float scale_unity = 1.f;
+
+constexpr AbsoluteTolerance<float> tolerance_qsymm16(1); /**< Tolerance value for comparing reference's output against implementation's output for 16-bit quantized symmetric data types */
+constexpr RelativeTolerance<float> tolerance_integer(1); /**< Tolerance value for comparing reference's output against implementation's output for integer data types */
+
 
 /** Tests for in-place computation
  * With current interface storing TensorInfo with quantization information
@@ -55,52 +61,206 @@ const float scale_other = 1.f / 32768.f;
  * expected to have either different quantization information, data type
  * or different shape we are not testing in-place computation.
  */
-const auto OutOfPlaceDataSet = framework::dataset::make("InPlace", {false});
+const auto OutOfPlaceDataSet = make("InPlace", {false});
+
+const auto PixelWiseMultiplicationPolicySTNUDataset =   combine(
+                                                            make("ConvertPolicy", { ConvertPolicy::SATURATE }),
+                                                            make("RoundingPolicy", { RoundingPolicy::TO_NEAREST_UP })
+                                                        );
+
+const auto PixelWiseMultiplicationPolicySTZDataset =    combine(
+                                                            make("ConvertPolicy", { ConvertPolicy::SATURATE }),
+                                                            make("RoundingPolicy", { RoundingPolicy::TO_ZERO })
+                                                        );
+
+const auto PixelWiseMultiplicationQASYMM8QuantDataset = combine(
+                                                            make("Src0QInfo", { QuantizationInfo(5.f / 32768.f, 0) }),
+                                                            make("Src1QInfo", { QuantizationInfo(2.f / 32768.f, 0) }),
+                                                            make("OutQInfo", { QuantizationInfo(1.f / 32768.f, 0) })
+                                                        );
 } // namespace
 
 using CpuMulU8U8toS16Fixture =
     CpuMulValidationFixture<Tensor, Accessor, experimental::op::CpuMul, uint8_t, uint8_t, int16_t>;
 
+using CpuMulQS8QS8QS8ThreadSafeFixture =
+    CpuMulQuantizedThreadSafeValidationFixture<Tensor, Accessor, experimental::op::CpuMul, int8_t, int8_t, int8_t>;
+
+using CpuMulQ16Q16S32ThreadSafeFixture =
+    CpuMulQuantizedThreadSafeValidationFixture<Tensor, Accessor, experimental::op::CpuMul, int16_t, int16_t, int32_t>;
+
+using CpuMulQ16Q16Q16ThreadSafeFixture =
+    CpuMulQuantizedThreadSafeValidationFixture<Tensor, Accessor, experimental::op::CpuMul, int16_t, int16_t, int16_t>;
+
+using CpuMulS32S32S32ThreadSafeFixture =
+    CpuMulThreadSafeValidationFixture<Tensor, Accessor, experimental::op::CpuMul, int32_t, int32_t, int32_t>;
+
+using CpuMulF32F32F32ThreadSafeFixture =
+    CpuMulThreadSafeValidationFixture<Tensor, Accessor, experimental::op::CpuMul, float, float, float>;
+
+const auto PixelWiseMultiplicationQSYMM16QuantDataset = combine(
+                                                                make("Src0QInfo", { QuantizationInfo(1.f / 32768.f, 0) }),
+                                                                make("Src1QInfo", { QuantizationInfo(2.f / 32768.f, 0) }),
+                                                                make("OutQInfo", { QuantizationInfo(5.f / 32768.f, 0) }));
 TEST_SUITE(NEON)
 TEST_SUITE(OPERATORS)
 TEST_SUITE(CpuMul)
 
 TEST_SUITE(U8U8toS16)
-FIXTURE_DATA_TEST_CASE(
-    SmokeTest0,
-    CpuMulU8U8toS16Fixture,
-    framework::DatasetMode::PRECOMMIT,
-    combine(combine(combine(combine(combine(combine(combine(datasets::SmallShapes(),
-                                                            framework::dataset::make("DataTypeIn1", DataType::U8)),
-                                                    framework::dataset::make("DataTypeIn2", DataType::U8)),
-                                            framework::dataset::make("DataTypeOut", DataType::S16)),
-                                    framework::dataset::make("Scale", {scale_255})),
-                            datasets::ConvertPolicies()),
-                    framework::dataset::make("RoundingPolicy", RoundingPolicy::TO_NEAREST_UP)),
-            OutOfPlaceDataSet))
+FIXTURE_DATA_TEST_CASE(SmokeTest0, CpuMulU8U8toS16Fixture, framework::DatasetMode::PRECOMMIT,
+    combine(
+        datasets::SmallShapes(),
+        make("DataTypeIn1", DataType::U8),
+        make("DataTypeIn2", DataType::U8),
+        make("DataTypeOut", DataType::S16),
+        make("Scale", {scale_255}),
+        datasets::ConvertPolicies(),
+        make("RoundingPolicy", RoundingPolicy::TO_NEAREST_UP),
+        OutOfPlaceDataSet
+    ))
 {
-    // Validate output
-    validate_wrap(Accessor(_target), _reference, AbsoluteTolerance<int16_t>(1), 0.f);
+    for(int i = 0; i < _num_parallel_runs; ++i)
+    {
+        // Validate output
+        validate_wrap(Accessor(_target[i]), _reference[i], AbsoluteTolerance<int16_t>(1), 0.f);
+    }
 }
 
-FIXTURE_DATA_TEST_CASE(
-    SmokeTest1,
-    CpuMulU8U8toS16Fixture,
-    framework::DatasetMode::PRECOMMIT,
-    combine(combine(combine(combine(combine(combine(combine(datasets::SmallShapes(),
-                                                            framework::dataset::make("DataTypeIn1", DataType::U8)),
-                                                    framework::dataset::make("DataTypeIn2", DataType::U8)),
-                                            framework::dataset::make("DataTypeOut", DataType::S16)),
-                                    framework::dataset::make("Scale", {scale_other})),
-                            datasets::ConvertPolicies()),
-                    framework::dataset::make("RoundingPolicy", RoundingPolicy::TO_ZERO)),
-            OutOfPlaceDataSet))
+FIXTURE_DATA_TEST_CASE(SmokeTest1, CpuMulU8U8toS16Fixture, framework::DatasetMode::PRECOMMIT,
+    combine(
+        datasets::SmallShapes(),
+        make("DataTypeIn1", DataType::U8),
+        make("DataTypeIn2", DataType::U8),
+        make("DataTypeOut", DataType::S16),
+        make("Scale", {scale_other}),
+        datasets::ConvertPolicies(),
+        make("RoundingPolicy", RoundingPolicy::TO_ZERO),
+        OutOfPlaceDataSet
+    ))
 {
     // Validate output
-    validate(Accessor(_target), _reference);
+    for(int i = 0; i < _num_parallel_runs; ++i)
+    {
+        validate(Accessor(_target[i]), _reference[i]);
+    }
 }
 TEST_SUITE_END() // U8U8toS16
 
+#ifndef BARE_METAL
+TEST_SUITE(ThreadSafety)
+TEST_SUITE(Quantized)
+TEST_SUITE(QASYMM8_SIGNED)
+FIXTURE_DATA_TEST_CASE(QS8QS8QS8, CpuMulQS8QS8QS8ThreadSafeFixture, framework::DatasetMode::ALL,
+    combine(
+        datasets::SmallShapes(),
+        make("DataTypeIn1", DataType::QASYMM8_SIGNED),
+        make("DataTypeIn2", DataType::QASYMM8_SIGNED),
+        make("DataTypeOut", DataType::QASYMM8_SIGNED),
+        make("Scale", { scale_255 }),
+        PixelWiseMultiplicationPolicySTNUDataset,
+        PixelWiseMultiplicationQASYMM8QuantDataset,
+        OutOfPlaceDataSet
+    ))
+
+{
+    // Validate output
+    for(int i = 0; i < _num_parallel_runs; ++i)
+    {
+        validate(Accessor(_target[i]), _reference[i]);
+    }
+}
+TEST_SUITE_END() // QASYMM8_SIGNED
+
+TEST_SUITE(QSYMM16)
+FIXTURE_DATA_TEST_CASE(Q16Q16S32, CpuMulQ16Q16S32ThreadSafeFixture, framework::DatasetMode::ALL,
+    combine(
+        datasets::SmallShapes(),
+        make("DataTypeIn1", DataType::QSYMM16),
+        make("DataTypeIn2", DataType::QSYMM16),
+        make("DataTypeOut", DataType::S32),
+        make("Scale", { scale_unity }),
+        PixelWiseMultiplicationPolicySTZDataset,
+        PixelWiseMultiplicationQSYMM16QuantDataset,
+        OutOfPlaceDataSet
+    ))
+
+{
+    // Validate output
+    for(int i = 0; i < _num_parallel_runs; ++i)
+    {
+        validate(Accessor(_target[i]), _reference[i]);
+    }
+}
+
+FIXTURE_DATA_TEST_CASE(Q16Q16Q16, CpuMulQ16Q16Q16ThreadSafeFixture, framework::DatasetMode::ALL,
+    combine(
+        datasets::SmallShapes(),
+        make("DataTypeIn1", DataType::QSYMM16),
+        make("DataTypeIn2", DataType::QSYMM16),
+        make("DataTypeOut", DataType::QSYMM16),
+        make("Scale", { scale_unity }),
+        PixelWiseMultiplicationPolicySTZDataset,
+        PixelWiseMultiplicationQSYMM16QuantDataset,
+        OutOfPlaceDataSet
+    ))
+
+{
+    // Validate output
+    for(int i = 0; i < _num_parallel_runs; ++i)
+    {
+        validate(Accessor(_target[i]), _reference[i], tolerance_qsymm16);
+    }
+}
+TEST_SUITE_END() // QSYMM16
+TEST_SUITE_END() // Quantized
+TEST_SUITE(INTEGER)
+TEST_SUITE(S32)
+FIXTURE_DATA_TEST_CASE(S32S32S32, CpuMulS32S32S32ThreadSafeFixture, framework::DatasetMode::PRECOMMIT,
+    combine(
+        datasets::SmallShapes(),
+        make("DataTypeIn1", DataType::S32),
+        make("DataTypeIn2", DataType::S32),
+        make("DataTypeOut", DataType::S32),
+        make("Scale", {scale_other}),
+        datasets::ConvertPolicies(),
+        make("RoundingPolicy", RoundingPolicy::TO_ZERO),
+        OutOfPlaceDataSet
+    ))
+{
+    // Validate output
+    for(int i = 0; i < _num_parallel_runs; ++i)
+    {
+        validate(Accessor(_target[i]), _reference[i], tolerance_integer);
+    }
+}
+TEST_SUITE_END() // S32
+TEST_SUITE_END() // INTEGER
+
+TEST_SUITE(Float)
+TEST_SUITE(F32)
+FIXTURE_DATA_TEST_CASE(F32F32F32, CpuMulF32F32F32ThreadSafeFixture, framework::DatasetMode::PRECOMMIT,
+    combine(
+        datasets::SmallShapes(),
+        make("DataTypeIn1", DataType::F32),
+        make("DataTypeIn2", DataType::F32),
+        make("DataTypeOut", DataType::F32),
+        make("Scale", {scale_other}),
+        datasets::ConvertPolicies(),
+        make("RoundingPolicy", RoundingPolicy::TO_ZERO),
+        OutOfPlaceDataSet
+    ))
+{
+    // Validate output
+    for(int i = 0; i < _num_parallel_runs; ++i)
+    {
+        validate(Accessor(_target[i]), _reference[i], tolerance_integer);
+    }
+}
+TEST_SUITE_END() // F32
+TEST_SUITE_END() // Float
+
+TEST_SUITE_END() // ThreadSafety
+#endif // #ifndef BARE_METAL
 TEST_SUITE_END() // CpuMul
 TEST_SUITE_END() // OPERATORS
 TEST_SUITE_END() // NEON
