34a2ef26ca1c8a6b9ae9d5e52f7e6f45d5d90dc1
Syed Wajahat Abbas Naqvi <syedwajahatabbas.naqvi@arm.com>
2025-06-10 15:44:37 +0200

    test: Add thread safety tests for CpuSoftmax
    Resolves: COMPMID-8329
    
    Change-Id: I3f74bc4c000c71a17fcca94c4552b8d97a61191a
    Signed-off-by: Syed Wajahat Abbas Naqvi <syedwajahatabbas.naqvi@arm.com>
    Reviewed-on: https://review.mlplatform.org/c/ml/ComputeLibrary/+/14698
    Tested-by: Arm Jenkins <bsgcomp@arm.com>
    Comments-Addressed: Arm Jenkins <bsgcomp@arm.com>
    Benchmark: Arm Jenkins <bsgcomp@arm.com>
    Reviewed-by: Dongsung Kim <dongsung.kim@arm.com>

--- tests/validation/fixtures/CpuSoftmaxFixture.h
+++ tests/validation/fixtures/CpuSoftmaxFixture.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2017-2021, 2023-2024 Arm Limited.
+ * Copyright (c) 2017-2021, 2023-2025 Arm Limited.
  *
  * SPDX-License-Identifier: MIT
  *
@@ -33,8 +33,12 @@
 #include "tests/framework/Asserts.h"
 #include "tests/framework/Fixture.h"
 #include "tests/validation/reference/SoftmaxLayer.h"
-
 #include <random>
+
+#if !defined(BARE_METAL)
+#include <thread>
+#include <vector>
+#endif // !defined(BARE_METAL)
 
 namespace arm_compute
 {
@@ -42,11 +46,16 @@ namespace test
 {
 namespace validation
 {
+namespace
+{
+constexpr int NUM_THREADS =  3;
+}// namespace
 template <typename TensorType, typename AccessorType, typename FunctionType, typename T, bool IS_LOG = false>
-class CpuSoftmaxValidationFixture : public framework::Fixture
+class CpuSoftmaxValidationGenericFixture : public framework::Fixture
 {
 public:
-    void setup(TensorShape shape, DataType data_type, float beta, size_t axis)
+    void setup(TensorShape shape, DataType data_type, float beta, size_t axis, QuantizationInfo qinfo,
+               TestType test_type = TestType::ConfigureOnceRunOnce)
     {
         if(std::is_same<TensorType, Tensor>::value &&  // Cpu
             data_type == DataType::F16 && !CPUInfo::get().has_fp16())
@@ -54,10 +63,12 @@ public:
             return;
         }
 
-        quantization_info_ = QuantizationInfo();
+        quantization_info_  = qinfo;
+        test_type_          = test_type;
+        num_parallel_runs_  = (test_type_ == TestType::ConfigureOnceRunMultiThreaded ? NUM_THREADS : 1);
 
-        reference_ = compute_reference(shape, data_type, quantization_info_, beta, axis);
-        target_    = compute_target(shape, data_type, quantization_info_, beta, axis);
+        compute_reference(shape, data_type, quantization_info_, beta, axis);
+        compute_target(shape, data_type, quantization_info_, beta, axis);
     }
 
 protected:
@@ -85,56 +96,140 @@ protected:
         }
     }
 
-    TensorType compute_target(const TensorShape &shape, DataType data_type,
+    void allocate_and_fill_tensors(TensorType *src, TensorType *dst){
+        for(int i = 0; i < num_parallel_runs_; ++i){
+
+            ARM_COMPUTE_ASSERT(src[i].info()->is_resizable());
+            ARM_COMPUTE_ASSERT(dst[i].info()->is_resizable());
+
+            // Allocate tensors
+            src[i].allocator()->allocate();
+            dst[i].allocator()->allocate();
+
+            ARM_COMPUTE_ASSERT(!src[i].info()->is_resizable());
+            ARM_COMPUTE_ASSERT(!dst[i].info()->is_resizable());
+
+            // Fill tensors
+            fill(AccessorType(src[i]));
+        }
+    }
+
+    void compute_target(const TensorShape &shape, DataType data_type,
                               QuantizationInfo quantization_info, float beta, int32_t axis)
     {
+        TensorType src[NUM_THREADS];
+        TensorType dst[NUM_THREADS];
+        ITensorPack run_pack[NUM_THREADS];
+        TensorType *dst_ptrs[NUM_THREADS];
+        auto mg = MemoryGroup{};
+
         // Create tensors
-        TensorType src = create_tensor<TensorType>(shape, data_type, 1, quantization_info);
-        TensorType dst = create_tensor<TensorType>(shape, data_type, 1, get_softmax_output_quantization_info(data_type, IS_LOG));
+        for(int i = 0; i < num_parallel_runs_; ++i){
+            src[i]  = create_tensor<TensorType>(shape, data_type, 1, quantization_info);
+            dst[i]  = create_tensor<TensorType>(shape, data_type, 1, get_softmax_output_quantization_info(data_type, IS_LOG));
+            dst_ptrs[i] = &dst[i];
+        }
 
         // Create and configure function
         FunctionType softmax;
-        softmax.configure(src.info(), dst.info(), beta, axis);
+        softmax.configure(src[0].info(), dst[0].info(), beta, axis);
 
-        ARM_COMPUTE_ASSERT(src.info()->is_resizable());
-        ARM_COMPUTE_ASSERT(dst.info()->is_resizable());
+        allocate_and_fill_tensors(src, dst);
 
-        // Allocate tensors
-        src.allocator()->allocate();
-        dst.allocator()->allocate();
+        if(test_type_ == TestType::ConfigureOnceRunMultiThreaded)
+        {
+#ifndef BARE_METAL
+            std::vector<std::thread> threads;
 
-        ARM_COMPUTE_ASSERT(!src.info()->is_resizable());
-        ARM_COMPUTE_ASSERT(!dst.info()->is_resizable());
+            threads.reserve(num_parallel_runs_);
+            for(int i = 0; i < num_parallel_runs_; ++i)
+            {
+                // Compute function
+                run_pack[i] = {{arm_compute::TensorType::ACL_SRC_0, &src[i]},
+                               {arm_compute::TensorType::ACL_DST, dst_ptrs[i]}};
 
-        // Fill tensors
-        fill(AccessorType(src));
+                threads.emplace_back([&,i]
+                {
+                    auto ws = manage_workspace<Tensor>(softmax.workspace(), mg, run_pack[i]);
+                    softmax.run(run_pack[i]);
+                    target_[i] = std::move(*(dst_ptrs[i]));
+                });
+            }
+            for(int i = 0; i < num_parallel_runs_; ++i)
+            {
+                threads[i].join();
+            }
+#endif // ifndef BARE_METAL
+        }
+        else
+        {
+            // Compute function
+            ITensorPack run_pack{{arm_compute::TensorType::ACL_SRC_0, &src[0]},
+                                 {arm_compute::TensorType::ACL_DST, dst_ptrs[0]}};
+            auto ws = manage_workspace<Tensor>(softmax.workspace(), mg, run_pack);
 
-        ITensorPack run_pack{ { arm_compute::TensorType::ACL_SRC_0, &src }};
-        run_pack.add_tensor(arm_compute::TensorType::ACL_DST, &dst);
-        auto mg = MemoryGroup{};
-        auto ws = manage_workspace<Tensor>(softmax.workspace(), mg, run_pack);
-
-        // Compute function
-        softmax.run(run_pack);
-
-        return dst;
+            // Compute function
+            softmax.run(run_pack);
+            target_[0] = std::move(*(dst_ptrs[0]));
+        }
     }
 
-    SimpleTensor<T> compute_reference(const TensorShape &shape, DataType data_type,
+    void compute_reference(const TensorShape &shape, DataType data_type,
                                       QuantizationInfo quantization_info, float beta, int32_t axis)
     {
         // Create reference
         SimpleTensor<T> src{ shape, data_type, 1, quantization_info };
 
         // Fill reference
-        fill(src);
-
-        return reference::softmax_layer<T>(src, beta, axis, IS_LOG);
+        for(int i = 0; i < num_parallel_runs_; ++i)
+        {
+            // Fill reference
+            fill(src);
+            reference_[i] = reference::softmax_layer<T>(src, beta, axis, IS_LOG);
+        }
     }
 
-    TensorType       target_{};
-    SimpleTensor<T>  reference_{};
+    TensorType       target_[NUM_THREADS];
+    SimpleTensor<T>  reference_[NUM_THREADS];
     QuantizationInfo quantization_info_{};
+    TestType         test_type_{};
+    int              num_parallel_runs_{};
+};
+
+template <typename TensorType, typename AccessorType, typename FunctionType, typename T>
+class CpuSoftmaxValidationFixture
+    : public CpuSoftmaxValidationGenericFixture<TensorType, AccessorType, FunctionType, T>
+{
+public:
+    void setup(TensorShape shape, DataType data_type, float beta, size_t axis)
+    {
+        CpuSoftmaxValidationGenericFixture<TensorType, AccessorType, FunctionType, T>::setup(
+            shape, data_type, beta, axis, QuantizationInfo(), TestType::ConfigureOnceRunOnce);
+    }
+};
+
+template <typename TensorType, typename AccessorType, typename FunctionType, typename T>
+class CpuSoftmaxThreadSafeValidationFixture
+    : public CpuSoftmaxValidationGenericFixture<TensorType, AccessorType, FunctionType, T>
+{
+public:
+    void setup(TensorShape shape, DataType data_type, float beta, size_t axis)
+    {
+        CpuSoftmaxValidationGenericFixture<TensorType, AccessorType, FunctionType, T>::setup(
+            shape, data_type, beta, axis, QuantizationInfo(), TestType::ConfigureOnceRunMultiThreaded);
+    }
+};
+
+template <typename TensorType, typename AccessorType, typename FunctionType, typename T>
+class CpuSoftmaxQuantizedThreadSafeValidationFixture
+    : public CpuSoftmaxValidationGenericFixture<TensorType, AccessorType, FunctionType, T>
+{
+public:
+    void setup(TensorShape shape, DataType data_type, float beta, size_t axis, QuantizationInfo qinfo)
+    {
+        CpuSoftmaxValidationGenericFixture<TensorType, AccessorType, FunctionType, T>::setup(
+            shape, data_type, beta, axis, qinfo, TestType::ConfigureOnceRunMultiThreaded);
+    }
 };
 
 } // namespace validation
